183e78ce3dda9b166aeff0cd3ab578ce4f26051c
ji-junhyuk <takeoutmoneywallet@naver.com>
2020-10-21 08:33:25 +0900

    스프링 기본 73/163

---
+++ TIL-TodayILearned/core/src/test/java/hello/core/singleton/SingletonServiceTest.java
@@ -0,0 +1,50 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class SingletonServiceTest {
+
+    @Test
+    @DisplayName("Using objects with singleton pattern applied.")
+    public void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+
+        singletonService1.logic();
+
+    }
+    //    new SingletonService();
+
+    @Test
+    @DisplayName("Spring container and singleton")
+    void springContainer() {
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
+
+    }
+
+
+}
---
+++ TIL-TodayILearned/core/src/test/java/hello/core/xml/XmlAppContent.java
@@ -0,0 +1,21 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class XmlAppContent {
+//
+//    @Test
+//    void xmlAppContent() {
+//        ApplicationContext ac = new GenericApplicationContext("appConfig.xml");
+//
+//        MemberService memberService = ac.getBean("memberService", MemberService.class);
+//
+//        assertThat(memberService).isInstanceOf(MemberService.class);
+//    }
+}
