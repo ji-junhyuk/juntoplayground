1319cc1f981c54838c35c08e341f46277beba294
ji-junhyuk <takeoutmoneywallet@naver.com>
2020-10-23 08:34:43 +0900

    JPA

--- TIL-TodayILearned/jpashop/src/main/java/jpabook/jpashop/domain/OrderSearch.java
+++ TIL-TodayILearned/jpashop/src/main/java/jpabook/jpashop/domain/OrderSearch.java
@@ -1,5 +1,9 @@
 package jpabook.jpashop.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class OrderSearch {
     private String memberName;
     private OrderStatus orderStatus;
