49eeafb567231e04437ca918f0e4f6077cec735d
ji-junhyuk <takeoutmoneywallet@naver.com>
2020-10-25 10:53:54 +0900

    Boot, Jpa 29/92

--- TIL-TodayILearned/jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ TIL-TodayILearned/jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -12,4 +12,12 @@ public class Address {
     private String street;
     private String zipcode;
 
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
