d63f813babc0bc44b5eb090878ced9be1c3a976c
ji-junhyuk <takeoutmoneywallet@naver.com>
2020-10-27 04:29:42 +0900

    Boot, JPA 61/92

--- TIL-TodayILearned/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ TIL-TodayILearned/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -9,6 +11,7 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id
@@ -23,6 +26,7 @@ public class OrderItem {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
+
     private int orderPrice;
     private int count;
 
