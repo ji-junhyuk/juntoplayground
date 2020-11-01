980914a826884e35cc1447726ae5cdedf85fbef2
ji-junhyuk <takeoutmoneywallet@naver.com>
2020-11-02 06:49:23 +0900

    Spring Basic 31/163

--- TIL-TodayILearned/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ TIL-TodayILearned/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -11,6 +11,7 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository = new MemoryMemberRepository();
     private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 
+//    private final DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
