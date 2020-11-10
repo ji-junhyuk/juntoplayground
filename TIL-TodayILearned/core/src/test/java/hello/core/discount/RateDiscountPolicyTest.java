847d1e8e7df70bf520a8b3775b8965f0c40768e2
ji-junhyuk <takeoutmoneywallet@naver.com>
2020-11-11 06:20:50 +0900

    Spring basics 54/163

---
+++ TIL-TodayILearned/core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,41 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP should have a 10% discount.")
+    public void vip_o() {
+        //given
+        Member member = new Member(1L, "memberVip", Grade.VIP);
+
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+
+        //then
+        assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("The discount should not be applied unless you are a VIP.")
+    public void vip_x() {
+        //given
+        Member member = new Member(2L, "memberBASIC", Grade.BASIC);
+
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+
+        //then
+        assertThat(discount).isEqualTo(0);
+    }
+}
