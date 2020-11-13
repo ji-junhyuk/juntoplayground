b9830bc6790fac5b7200bd6feda98b520c36cac4
ji-junhyuk <takeoutmoneywallet@naver.com>
2020-11-13 19:20:33 +0900

    Toby 58/857

--- TIL-TodayILearned/java8/src/main/java/_11_11/java8/Foo.java
+++ TIL-TodayILearned/java8/src/main/java/_11_11/java8/Foo.java
@@ -1,65 +1,18 @@
 package _11_11.java8;
 
+import java.util.function.Function;
+
 public class Foo {
 
     public static void main(String[] args) {
+        Function<Integer, Integer> plus10 = (i) -> i + 10;
+        Function<Integer, Integer> multiply2 = (i) -> i * 2;
 
-        RunSomething runSomething = new RunSomething() {
-
-            int baseNumber = 10;
-
-            @Override
-            public int doit(int number) {
-                return number + baseNumber;
-            }
-        };
-//        If necessary, functional programming is done within the allowable grammar. It may be okay to use it for convenience.
-//        Lambda cannot be used when changing the state value. However, lambdas can be used when only referencing values that are outside
-//        There is no side effect. (It does not change the value outside the function.)
-//        There is no state. (Do not use values outside the function.)
-
-/*
-     RunSomething runSomething = (number) -> number + 10;
-
-        RunSomething runSomething = (number) -> {
-            return number + 10;
-        };
-
-
-
-
-        System.out.println(runSomething.doit(1));
-        System.out.println(runSomething.doit(1));
-        System.out.println(runSomething.doit(1));
-
-        System.out.println(runSomething.doit(2));
-        System.out.println(runSomething.doit(2));
-        System.out.println(runSomething.doit(2));
-*/
-
-        /*
-        If the value is not constant or is likely to change
-        => Difficult to see as functional programming
-
-        11 11 11 12 12 12
-         */
+        Function<Integer, Integer> multiply2AndPlus10 = plus10.compose(multiply2);
+        plus10.andThen(multiply2);
     }
 }
-
-
-
-        //Prior to Java 8, anonymous inner class
 /*
-        RunSomething runSomething = () -> System.out.println("Hello");
-        runSomething.doIt();
+        Plus10 plus10 = new Plus10();
+        System.out.println(plus10.apply(10));
 */
-
-    /*
-        RunSomething runSomething = new RunSomething() {
-            @Override
-            public void doIt() {
-                System.out.println("Hello");
-            }
-        };
-    */
-
