18fb3553d77e12b1a93720c5881931f2508dd029
ji-junhyuk <takeoutmoneywallet@naver.com>
2020-11-14 05:23:56 +0900

    Spring basics 66/163

---
+++ TIL-TodayILearned/core/src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -0,0 +1,23 @@
+package hello.core.beandefinition;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BeanDefinitionTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinition = " + beanDefinition + "beanDefinitionName" + beanDefinitionName);
+            }
+        }
+    }
+}
