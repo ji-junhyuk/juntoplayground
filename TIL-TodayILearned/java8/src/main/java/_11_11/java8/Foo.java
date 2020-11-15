c129076f18912e966ec60a6864f95f10d3a60937
ji-junhyuk <takeoutmoneywallet@naver.com>
2020-11-15 11:07:52 +0900

    modify foo

--- TIL-TodayILearned/java8/src/main/java/_11_11/java8/Foo.java
+++ TIL-TodayILearned/java8/src/main/java/_11_11/java8/Foo.java
@@ -1,18 +1,21 @@
 package _11_11.java8;
 
-import java.util.function.Function;
+import java.util.function.*;
 
 public class Foo {
 
     public static void main(String[] args) {
-        Function<Integer, Integer> plus10 = (i) -> i + 10;
-        Function<Integer, Integer> multiply2 = (i) -> i * 2;
+        Foo foo = new Foo();
+        foo.run();
+    }
 
-        Function<Integer, Integer> multiply2AndPlus10 = plus10.compose(multiply2);
-        plus10.andThen(multiply2);
+    private void run() {
+        final int baseNumber = 10;
+        
+        IntConsumer printInt = (i) -> {
+            System.out.println(i + baseNumber);
+        };
+
+        printInt.accept(10);
     }
 }
-/*
-        Plus10 plus10 = new Plus10();
-        System.out.println(plus10.apply(10));
-*/
