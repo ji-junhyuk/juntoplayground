fa2c27760b9af3ebd1a5751dacfc6c261493e678
ji-junhyuk <takeoutmoneywallet@naver.com>
2020-11-16 09:56:15 +0900

    Spring orm jpa (6) 9/54 (45m)

--- TIL-TodayILearned/orm/src/main/java/hellojpa/orm/OrmApplication.java
+++ TIL-TodayILearned/orm/src/main/java/hellojpa/orm/OrmApplication.java
@@ -11,24 +11,4 @@ import javax.persistence.Persistence;
 @SpringBootApplication
 public class OrmApplication {
 
-	public static void main(String[] args) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-
-		EntityManager em = emf.createEntityManager();
-
-		EntityTransaction tx = em.getTransaction();
-		tx.begin();
-
-		Member member = new Member();
-		member.setId(2L);
-		member.setName("HelloB");
-
-		tx.commit();
-
-		em.persist(member);
-
-		em.close();
-
-		emf.close();
-	}
 }
