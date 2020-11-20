b39427b9dd0ce903240aa88c6ac5737ddf394d52
ji-junhyuk <takeoutmoneywallet@naver.com>
2020-11-20 11:45:59 +0900

    Jpa1, (23) 34/93 (30m)

--- TIL-TodayILearned/jpashop/src/main/java/jpabook/jpashop/domain/item/Book.java
+++ TIL-TodayILearned/jpashop/src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -1,4 +1,17 @@
 package jpabook.jpashop.domain.item;
 
-public class Book {
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("Book")
+@Getter
+@Setter
+public class Book extends Item {
+
+    private String author;
+    private String isbn;
 }
--- TIL-TodayILearned/jpashop/src/main/java/jpabook/jpashop/domain/item/Movie.java
+++ TIL-TodayILearned/jpashop/src/main/java/jpabook/jpashop/domain/item/Movie.java
@@ -1,4 +1,17 @@
 package jpabook.jpashop.domain.item;
 
-public class Movie {
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("Movie")
+@Getter
+@Setter
+public class Movie extends Item {
+
+    private String actor;
+    private String director;
 }
