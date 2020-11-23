999b43a63c6f97bb61882f3ceb9b0434cf8e1689
ji-junhyuk <takeoutmoneywallet@naver.com>
2020-11-23 10:15:08 +0900

    JPA1, (32) 41/93 (40m)

--- TIL-TodayILearned/JPA1/src/main/java/hellospring/JPA1/service/MemberService.java
+++ TIL-TodayILearned/JPA1/src/main/java/hellospring/JPA1/service/MemberService.java
@@ -1,4 +1,48 @@
 package hellospring.JPA1.service;
 
+import hellospring.JPA1.domain.Member;
+import hellospring.JPA1.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /*
+    Sign up
+     */
+
+    @Transactional
+    public Long join(Member member) {
+
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("Already existing member");
+        }
+    }
+
+    /*
+    View all member
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
 }
---
+++ TIL-TodayILearned/JPA1/src/test/java/hellospring/JPA1/service/MemberServiceTest.java
@@ -0,0 +1,53 @@
+package hellospring.JPA1.service;
+
+import hellospring.JPA1.domain.Member;
+import hellospring.JPA1.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void join() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+    @Test
+    public void duplicateMemberException() throws Exception {
+/*
+        //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        //when
+        memberService.join(member1);
+        memberService.join(member2);
+
+        //then
+        fail("error should occur.");
+*/
+     }
+}
