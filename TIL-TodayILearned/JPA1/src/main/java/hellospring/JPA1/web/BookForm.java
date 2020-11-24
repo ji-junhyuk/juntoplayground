9302b43e4bcde8ffd08196eef6fafade0e57b31c
ji-junhyuk <takeoutmoneywallet@naver.com>
2020-11-24 10:13:57 +0900

    Jpa1, (63) 75/93 (50m)

--- TIL-TodayILearned/JPA1/src/main/java/hellospring/JPA1/web/BookForm.java
+++ TIL-TodayILearned/JPA1/src/main/java/hellospring/JPA1/web/BookForm.java
@@ -1,4 +1,19 @@
 package hellospring.JPA1.web;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class BookForm {
+
+    private Long id;
+
+    private String name;
+    private int price;
+
+    private int stockQuantity;
+
+    private String author;
+    private String isbn;
 }
--- TIL-TodayILearned/JPA1/src/main/java/hellospring/JPA1/web/MemberForm.java
+++ TIL-TodayILearned/JPA1/src/main/java/hellospring/JPA1/web/MemberForm.java
@@ -1,4 +1,19 @@
 package hellospring.JPA1.web;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
 public class MemberForm {
+
+    @NotEmpty(message = "Member name is necessary")
+    private String name;
+
+    private String city;
+    private String street;
+    private String zipcode;
+
 }
