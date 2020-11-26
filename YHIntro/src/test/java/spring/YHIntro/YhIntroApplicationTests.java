5e3aeb2fbe2987cff1340b1c10e6e1c636d75bb9
junto <takeoutmoneywallet@naver.com>
2020-11-27 06:29:48 +0900

    chore: Complete spring web development settings

---
+++ YHIntro/src/test/java/spring/YHIntro/YhIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.YHIntro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YhIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
