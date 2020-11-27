79207a9750abb2a3129ea57697c3b76490d4eda6
junto <takeoutmoneywallet@naver.com>
2020-11-28 06:30:33 +0900

    refactor: Remove duplicate and Make a role visible

--- YHBasic/src/main/java/spring/YHBasic/AppConfig.java
+++ YHBasic/src/main/java/spring/YHBasic/AppConfig.java
@@ -1,6 +1,8 @@
 package spring.YHBasic;
 
+import spring.YHBasic.discount.DiscountPolicy;
 import spring.YHBasic.discount.FixDiscountPolicy;
+import spring.YHBasic.repository.MemberRepository;
 import spring.YHBasic.repository.MemoryMemberRepository;
 import spring.YHBasic.service.MemberService;
 import spring.YHBasic.service.MemberServiceImpl;
@@ -10,12 +12,24 @@ import spring.YHBasic.service.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    public MemberRepository memberRepository() {
+        return MemberRepository();
+    }
+
+    public MemberRepository MemberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
         return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy());
+                memberRepository(),
+                discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
