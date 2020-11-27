a8d8ed205f5f6669d2de711e69f2324870e9e40f
junto <takeoutmoneywallet@naver.com>
2020-11-27 17:29:42 +0900

    feat: Register a new member

--- YHIntro/src/main/java/spring/YHIntro/controller/MemberController.java
+++ YHIntro/src/main/java/spring/YHIntro/controller/MemberController.java
@@ -2,6 +2,9 @@ package spring.YHIntro.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import spring.YHIntro.domain.Member;
 import spring.YHIntro.domain.MemberService;
 
 @Controller
@@ -9,4 +12,20 @@ import spring.YHIntro.domain.MemberService;
 public class MemberController {
 
     private final MemberService memberService;
+
+    @GetMapping(value = "/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String create(MemberForm form) {
+
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
 }
---
+++ YHIntro/src/main/java/spring/YHIntro/controller/MemberForm.java
@@ -0,0 +1,10 @@
+package spring.YHIntro.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberForm {
+    private String name;
+}
