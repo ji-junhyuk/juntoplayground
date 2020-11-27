4bf4162b4adb46332cfc8519ce9c2011cfb9f9e5
junto <takeoutmoneywallet@naver.com>
2020-11-28 05:08:12 +0900

    chore: Complete spring web development settings

---
+++ YHBasic/src/test/java/spring/YHBasic/YhBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.YHBasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YhBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
