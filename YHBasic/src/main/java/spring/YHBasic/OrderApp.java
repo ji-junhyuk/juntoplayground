c5563c00325ade107ff2e1f98ec8d3cd1b972d85
junto <takeoutmoneywallet@naver.com>
2020-11-28 05:55:49 +0900

    test: Test a OrderService(according to member, differ to price)

---
+++ YHBasic/src/main/java/spring/YHBasic/OrderApp.java
@@ -0,0 +1,25 @@
+package spring.YHBasic;
+
+import spring.YHBasic.domain.Grade;
+import spring.YHBasic.domain.Member;
+import spring.YHBasic.domain.Order;
+import spring.YHBasic.service.MemberService;
+import spring.YHBasic.service.MemberServiceImpl;
+import spring.YHBasic.service.OrderService;
+import spring.YHBasic.service.OrderServiceImpl;
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order);
+    }
+}
