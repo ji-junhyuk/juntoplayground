8923054c907533142da26405806a7db245d26411
junto <takeoutmoneywallet@naver.com>
2020-11-29 08:52:06 +0900

    feat : Add delivery entity(id, order, address, status)

--- YHJpa/src/main/java/spring/YHJpa/domain/Delivery.java
+++ YHJpa/src/main/java/spring/YHJpa/domain/Delivery.java
@@ -3,8 +3,24 @@ package spring.YHJpa.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
 public class Delivery {
 
+    @Id
+    @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
     private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status;
 }
