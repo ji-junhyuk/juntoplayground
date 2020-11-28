3204b0a607279f65614ea302ca61041235aeb85f
junto <takeoutmoneywallet@naver.com>
2020-11-29 05:47:33 +0900

    chore: Set spring web development settings

---
+++ YHJpa/src/main/java/spring/YHJpa/YhJpaApplication.java
@@ -0,0 +1,13 @@
+package spring.YHJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YhJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YhJpaApplication.class, args);
+	}
+
+}
---
+++ YHJpa/src/test/java/spring/YHJpa/YhJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.YHJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YhJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
