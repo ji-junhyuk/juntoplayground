a1478a3809f25f450a2bee99a7fdb7541955308f
junto <takeoutmoneywallet@naver.com>
2020-11-29 12:22:55 +0900

    feat: Throw NotEnoughStockException

--- YHJpa/src/main/java/spring/YHJpa/domain/item/Item.java
+++ YHJpa/src/main/java/spring/YHJpa/domain/item/Item.java
@@ -35,7 +35,7 @@ public abstract class Item {
     public void removeStock(int quantity) {
         int restStock = this.stockQuantity - quantity;
         if (restStock < 0) {
-            NotEnoughStockException("need more stock");
+            throw new NotEnoughStockException("need more stock");
         }
         this.stockQuantity = restStock;
     }
