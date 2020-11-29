d00d0319677eeb2f87bffc405824e53c4ede18e4
junto <takeoutmoneywallet@naver.com>
2020-11-29 12:29:17 +0900

    feat: Add ItemService function(save, findAll, findOne = Just delegate)

--- YHJpa/src/main/java/spring/YHJpa/service/ItemService.java
+++ YHJpa/src/main/java/spring/YHJpa/service/ItemService.java
@@ -1,4 +1,30 @@
 package spring.YHJpa.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import spring.YHJpa.domain.item.Item;
+import spring.YHJpa.repository.ItemRepository;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
 }
