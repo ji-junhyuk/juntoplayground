ecf1adc4a0e429e909e2b12528232ddc38c557dd
junto <takeoutmoneywallet@naver.com>
2020-11-30 06:33:18 +0900

    feat: Add member enrollment form and controller

--- YHIntro/src/main/java/spring/YHIntro/web/MemberController.java
+++ YHIntro/src/main/java/spring/YHIntro/web/MemberController.java
@@ -2,6 +2,7 @@ package spring.YHIntro.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import spring.YHIntro.service.MemberService;
 
 @Controller
@@ -12,5 +13,10 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping(value = "/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
     }
 }
