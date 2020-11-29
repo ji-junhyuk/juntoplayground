458f540d6f42dbbbd9f0655ad40f5327b0ee36c6
junto <takeoutmoneywallet@naver.com>
2020-11-30 06:41:43 +0900

    test: Run a SpringIntegrationTest(springBootTest, transactional)

---
+++ YHIntro/src/test/java/spring/YHIntro/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,53 @@
+package spring.YHIntro.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import spring.YHIntro.domain.Member;
+import spring.YHIntro.repository.MemoryMemberRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Test
+    public void join() throws Exception {
+
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberRepository.findById(saveId).get();
+        assertEquals(member.getName(), findMember.getName());
+    }
+
+    @Test
+    public void duplicateMemberException() throws Exception {
+
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));
+
+        //then
+        assertThat(e.getMessage()).isEqualTo("Already existing member.");
+
+    }
+}
