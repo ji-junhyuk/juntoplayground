38d47e7b1e618979faf266a5ce1aaa4b565f6b26
junto <takeoutmoneywallet@naver.com>
2020-11-29 14:46:58 +0900

    feat: Enroll item

--- YHJpa/src/main/java/spring/YHJpa/web/ItemController.java
+++ YHJpa/src/main/java/spring/YHJpa/web/ItemController.java
@@ -1,4 +1,37 @@
 package spring.YHJpa.web;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import spring.YHJpa.domain.item.Book;
+import spring.YHJpa.service.ItemService;
+
+@Controller
+@RequiredArgsConstructor
 public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping(value = "/items/new")
+    public String createForm(Model model) {
+
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping(value = "/items/new")
+    public String create(BookForm form) {
+
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/items";
+    }
 }
