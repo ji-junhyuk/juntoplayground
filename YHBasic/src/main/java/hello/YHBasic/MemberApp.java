7bf60e17b5bea6776c600ffa71080b74e201e3dc
junto <takeoutmoneywallet@naver.com>
2020-12-01 06:09:09 +0900

    feat: Use Appconfig in MemberApp & OrderApp

--- YHBasic/src/main/java/hello/YHBasic/MemberApp.java
+++ YHBasic/src/main/java/hello/YHBasic/MemberApp.java
@@ -8,7 +8,9 @@ import hello.YHBasic.service.MemberServiceImpl;
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
