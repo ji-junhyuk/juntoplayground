3565af576f0fa55d3ccc88b35ef4f7c4a8e07adb
junto <takeoutmoneywallet@naver.com>
2020-12-01 05:15:46 +0900

    feat: Implemnet a discountPolicy(if you are vip, 1000won discount)

---
+++ YHBasic/src/main/java/hello/YHBasic/discount/FixDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.YHBasic.discount;
+
+import hello.YHBasic.domain.Grade;
+import hello.YHBasic.domain.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+
+    private int discountFixAmount = 1000;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return discountFixAmount;
+        } else {
+            return 0;
+        }
+    }
+}
