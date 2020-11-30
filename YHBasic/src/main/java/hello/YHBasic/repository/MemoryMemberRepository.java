c0d6b9fcc9143f1f09b72e3e4fe31f29f9ab0d42
junto <takeoutmoneywallet@naver.com>
2020-12-01 04:54:16 +0900

    feat: Implement memberRepository

---
+++ YHBasic/src/main/java/hello/YHBasic/repository/MemoryMemberRepository.java
@@ -0,0 +1,21 @@
+package hello.YHBasic.repository;
+
+import hello.YHBasic.domain.Member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
