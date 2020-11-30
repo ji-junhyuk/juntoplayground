91a22a2a9efb966e92f67b4c58e63c5aed6eaad5
junto <takeoutmoneywallet@naver.com>
2020-12-01 04:34:10 +0900

    chore: Set a web development settings

--- YHBasic/src/main/java/hello/YHBasic/YhBasicApplication.java
+++ YHBasic/src/main/java/hello/YHBasic/YhBasicApplication.java
@@ -1,4 +1,4 @@
-package spring.YHBasic;
+package hello.YHBasic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- YHBasic/src/main/java/spring/YHBasic/domain/Grade.java
+++
@@ -1,5 +0,0 @@
-package spring.YHBasic.domain;
-
-public enum Grade {
-    BASIC, VIP
-}
