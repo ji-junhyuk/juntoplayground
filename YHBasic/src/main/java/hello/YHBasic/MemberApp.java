4b79290811f49b7a5970b03c8659986cd0b5334b
junto <takeoutmoneywallet@naver.com>
2020-12-01 06:21:16 +0900

    refactor: Convert pure java code to spring DI container

--- YHBasic/src/main/java/hello/YHBasic/MemberApp.java
+++ YHBasic/src/main/java/hello/YHBasic/MemberApp.java
@@ -4,12 +4,14 @@ import hello.YHBasic.domain.Grade;
 import hello.YHBasic.domain.Member;
 import hello.YHBasic.service.MemberService;
 import hello.YHBasic.service.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
