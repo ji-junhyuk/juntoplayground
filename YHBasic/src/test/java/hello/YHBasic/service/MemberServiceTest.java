2d0e921fd3a7bfe8554c1dd2329ed693998f9668
junto <takeoutmoneywallet@naver.com>
2020-12-01 05:04:33 +0900

    test: Test a MemberService in Junit5

---
+++ YHBasic/src/test/java/hello/YHBasic/service/MemberServiceTest.java
@@ -0,0 +1,28 @@
+package hello.YHBasic.service;
+
+import hello.YHBasic.domain.Grade;
+import hello.YHBasic.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    public void join() {
+
+        //given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+
+}
