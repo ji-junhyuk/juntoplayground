f78d537a864a247e64848e5685eeb13b18486d49
junto <takeoutmoneywallet@naver.com>
2020-12-01 06:07:30 +0900

    feat: Add a constructor(MemberServiceImpl, OrderServiceImpl)

--- YHBasic/src/main/java/hello/YHBasic/service/OrderServiceImpl.java
+++ YHBasic/src/main/java/hello/YHBasic/service/OrderServiceImpl.java
@@ -9,8 +9,13 @@ import hello.YHBasic.repository.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
