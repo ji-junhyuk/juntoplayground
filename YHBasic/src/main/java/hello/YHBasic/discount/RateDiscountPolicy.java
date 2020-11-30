1673204ae6f222d67f19e28707822bd2888c4560
junto <takeoutmoneywallet@naver.com>
2020-12-01 05:53:11 +0900

    feat: Implement discountPolicy(Rate 10%)

---
+++ YHBasic/src/main/java/hello/YHBasic/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.YHBasic.discount;
+
+import hello.YHBasic.domain.Grade;
+import hello.YHBasic.domain.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
