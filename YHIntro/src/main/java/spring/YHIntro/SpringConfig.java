30c17fea4a95d05916dd426c1fa2d10177664eb8
junto <takeoutmoneywallet@naver.com>
2020-11-30 09:51:53 +0900

    feat: Map jpa entity(Member)

--- YHIntro/src/main/java/spring/YHIntro/SpringConfig.java
+++ YHIntro/src/main/java/spring/YHIntro/SpringConfig.java
@@ -2,12 +2,21 @@ package spring.YHIntro;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import spring.YHIntro.repository.JdbcTemplateMemberRepository;
 import spring.YHIntro.repository.MemberRepository;
 import spring.YHIntro.repository.MemoryMemberRepository;
 import spring.YHIntro.service.MemberService;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +25,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
