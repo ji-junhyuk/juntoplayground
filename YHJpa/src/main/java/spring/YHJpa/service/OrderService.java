a6b4df5887a4d8e0241ef8f2d88cd5e909838d0c
junto <takeoutmoneywallet@naver.com>
2020-12-02 05:55:01 +0900

    feat: Add search order method to OrderService

--- YHJpa/src/main/java/spring/YHJpa/service/OrderService.java
+++ YHJpa/src/main/java/spring/YHJpa/service/OrderService.java
@@ -9,6 +9,8 @@ import spring.YHJpa.repository.ItemRepository;
 import spring.YHJpa.repository.MemberRepository;
 import spring.YHJpa.repository.OrderRepository;
 
+import java.util.List;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -53,5 +55,12 @@ public class OrderService {
         Order order = orderRepository.findOne(orderId);
         //Cancel order
         order.cancel();
+    }
+
+    /*
+    Search order
+     */
+    public List<Order> findOrders(OrderSearch orderSearch) {
+        return orderRepository.findAllByString(orderSearch);
     }
 }
