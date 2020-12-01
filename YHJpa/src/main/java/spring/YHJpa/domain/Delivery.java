73519c59f62f39b03ebc4cdc5081c8ebd91cd140
junto <takeoutmoneywallet@naver.com>
2020-12-02 03:46:28 +0900

    feat: Add a association method(order) - member, orderitem, delivery

--- YHJpa/src/main/java/spring/YHJpa/domain/Delivery.java
+++ YHJpa/src/main/java/spring/YHJpa/domain/Delivery.java
@@ -1,4 +1,11 @@
 package spring.YHJpa.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Delivery {
+
+    private Order order;
 }
--- YHJpa/src/main/java/spring/YHJpa/domain/OrderItem.java
+++ YHJpa/src/main/java/spring/YHJpa/domain/OrderItem.java
@@ -1,4 +1,10 @@
 package spring.YHJpa.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class OrderItem {
+
+    private Order order;
 }
