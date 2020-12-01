487a0d43d0c0a7486c28a7ac3f5b83f9b98640d9
junto <takeoutmoneywallet@naver.com>
2020-12-02 05:41:50 +0900

    feat: Add createOrderItem(item, orderprice, count)

--- YHJpa/src/main/java/spring/YHJpa/domain/OrderItem.java
+++ YHJpa/src/main/java/spring/YHJpa/domain/OrderItem.java
@@ -28,6 +28,17 @@ public class OrderItem {
 
     private int count;
 
+    //==Create Method==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
     //==Business Logic==//
     public void cancel() {
         getItem().addStock(count);
