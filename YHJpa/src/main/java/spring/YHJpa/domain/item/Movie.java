8366e280835939890776ade4697c7ea62cda6716
junto <takeoutmoneywallet@naver.com>
2020-12-02 04:16:36 +0900

    feat: Set a child(album, book, movie)

---
+++ YHJpa/src/main/java/spring/YHJpa/domain/item/Movie.java
@@ -0,0 +1,17 @@
+package spring.YHJpa.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("movie")
+@Getter
+@Setter
+public class Movie extends Item {
+
+    private String director;
+    private String actor;
+}
