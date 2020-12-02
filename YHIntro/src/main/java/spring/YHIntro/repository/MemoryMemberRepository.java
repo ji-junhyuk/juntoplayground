2d68fa8b617fbdbd7a5945b77aea228e8aea563e
junto <takeoutmoneywallet@naver.com>
2020-12-03 05:25:46 +0900

    feat: Use component scan

--- YHIntro/src/main/java/spring/YHIntro/repository/MemoryMemberRepository.java
+++ YHIntro/src/main/java/spring/YHIntro/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package spring.YHIntro.repository;
 
+import org.springframework.stereotype.Repository;
 import spring.YHIntro.domain.Member;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap();
--- YHIntro/src/main/java/spring/YHIntro/service/MemberService.java
+++ YHIntro/src/main/java/spring/YHIntro/service/MemberService.java
@@ -1,5 +1,7 @@
 package spring.YHIntro.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import spring.YHIntro.domain.Member;
 import spring.YHIntro.repository.MemberRepository;
 import spring.YHIntro.repository.MemoryMemberRepository;
@@ -7,10 +9,12 @@ import spring.YHIntro.repository.MemoryMemberRepository;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
