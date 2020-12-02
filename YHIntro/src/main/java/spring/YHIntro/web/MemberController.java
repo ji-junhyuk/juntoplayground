b8f8444ab65470e09b4947b2f72f9d3457d6ca2f
junto <takeoutmoneywallet@naver.com>
2020-12-03 05:32:58 +0900

    feat: Add function(Member Enroll)

--- YHIntro/src/main/java/spring/YHIntro/web/MemberController.java
+++ YHIntro/src/main/java/spring/YHIntro/web/MemberController.java
@@ -2,6 +2,8 @@ package spring.YHIntro.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import spring.YHIntro.domain.Member;
 import spring.YHIntro.service.MemberService;
 
 @Controller
@@ -16,5 +18,16 @@ public class MemberController {
     @GetMapping(value = "/members/new")
     public String createForm() {
         return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String create(MemberForm form) {
+
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
