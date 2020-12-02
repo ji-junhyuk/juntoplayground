b382e83c2009824e136ee27ce964f38310f8cd64
ji-junhyuk <67992469+ji-junhyuk@users.noreply.github.com>
2020-12-03 06:17:23 +0900

    refactor: Correct typo(Add autowired)

--- YHIntro/src/main/java/spring/YHIntro/web/MemberController.java
+++ YHIntro/src/main/java/spring/YHIntro/web/MemberController.java
@@ -14,6 +14,7 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
