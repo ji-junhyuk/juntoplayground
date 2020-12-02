9e660427b53903147884600ace3fd12ae1927f57
junto <takeoutmoneywallet@naver.com>
2020-12-03 05:31:12 +0900

    feat: Use a memberEnrollment form

--- YHIntro/src/main/java/spring/YHIntro/web/MemberController.java
+++ YHIntro/src/main/java/spring/YHIntro/web/MemberController.java
@@ -1,6 +1,7 @@
 package spring.YHIntro.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import spring.YHIntro.service.MemberService;
 
 @Controller
@@ -10,5 +11,10 @@ public class MemberController {
 
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping(value = "/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
     }
 }
