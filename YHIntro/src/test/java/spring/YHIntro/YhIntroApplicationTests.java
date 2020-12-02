426d7bff8fc058e19187e91657ed4f0db2f81473
junto <takeoutmoneywallet@naver.com>
2020-12-03 04:33:50 +0900

    chore: Set a web development settings

--- YHIntro/src/test/java/spring/YHIntro/YhIntroApplicationTests.java
+++ YHIntro/src/test/java/spring/YHIntro/YhIntroApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class YHIntroApplicationTests {
+class YhIntroApplicationTests {
 
 	@Test
 	void contextLoads() {
