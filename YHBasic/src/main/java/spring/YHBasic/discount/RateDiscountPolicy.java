988d5bded9a5a0740080ace8c758e982c9e96227
junto <takeoutmoneywallet@naver.com>
2020-12-04 06:20:10 +0900

    feat: Set a RateDiscountPolicy

---
+++ YHBasic/src/main/java/spring/YHBasic/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package spring.YHBasic.discount;
+
+import spring.YHBasic.domain.Grade;
+import spring.YHBasic.domain.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
