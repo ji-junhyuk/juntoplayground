fec6f768b176e9cc858ab8e2b2fd9c5778a5e6aa
junto <takeoutmoneywallet@naver.com>
2020-12-04 05:47:57 +0900

    feat: Set a memberRepository(save, findById)

---
+++ YHBasic/src/main/java/spring/YHBasic/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package spring.YHBasic.repository;
+
+import spring.YHBasic.domain.Member;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+
+    Member findById(Long memberId);
+}
