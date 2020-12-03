74b132d30cdb3effa8e626043d920d5dfb0a36f2
junto <takeoutmoneywallet@naver.com>
2020-12-03 21:05:32 +0900

    Chore: Set a C development settings

--- C/main.c
+++ C/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main()
+{
+    char alpha;
+
+    printf("알파벳 입력: ");
+    scanf("%c", &alpha);
+    printf("알파벳 %d은(는) 10진수로 %d이다.", alpha, alpha);
+
     return 0;
 }
