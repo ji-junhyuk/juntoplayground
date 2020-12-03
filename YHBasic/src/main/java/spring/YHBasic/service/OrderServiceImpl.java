ac6b161530b2fe8b9c28fb3406c9ce12ef6b0b34
junto <takeoutmoneywallet@naver.com>
2020-12-04 06:10:47 +0900

    feat: Implement a OrderService

---
+++ YHBasic/src/main/java/spring/YHBasic/service/OrderServiceImpl.java
@@ -0,0 +1,23 @@
+package spring.YHBasic.service;
+
+import spring.YHBasic.discount.DiscountPolicy;
+import spring.YHBasic.discount.FixDiscountPolicy;
+import spring.YHBasic.domain.Member;
+import spring.YHBasic.domain.Order;
+import spring.YHBasic.repository.MemberRepository;
+import spring.YHBasic.repository.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
