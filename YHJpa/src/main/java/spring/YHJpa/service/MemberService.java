d3b433d293dd527181a0c81fbf778e46858cd92e
junto <takeoutmoneywallet@naver.com>
2020-12-04 22:22:06 +0900

    feat: Add validateDuplicateName & PwdMember & logingo

--- YHJpa/src/main/java/spring/YHJpa/service/MemberService.java
+++ YHJpa/src/main/java/spring/YHJpa/service/MemberService.java
@@ -23,14 +23,31 @@ public class MemberService {
     public Long join(Member member) {
 
         validateDuplicateMember(member);
+        validateDuplicateName(member);
+        validatePwdMember(member);
         memberRepository.save(member);
-        return member.getId();
+        return member.getNumber();
     }
 
     private void validateDuplicateMember(Member member) {
         List<Member> findMembers = memberRepository.findByName(member.getName());
         if (!findMembers.isEmpty()) {
             throw new IllegalStateException("Already existing member.");
+        }
+    }
+
+    private void validateDuplicateName(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("the name is already in use.");
+        }
+    }
+
+    private void validatePwdMember(Member member) {
+        String pwdCheck = member.getPwd();
+
+        if (!pwdCheck.equals(member.getPwd2())) {
+            throw new IllegalStateException("Passwords do not match");
         }
     }
 
@@ -43,5 +60,23 @@ public class MemberService {
 
     public Member findOne(Long memberId) {
         return memberRepository.findOne(memberId);
+    }
+
+    /*
+    Check member existence
+     */
+    public boolean logingo(Member member) {
+
+        boolean check = false;
+
+        List<Member> sql = memberRepository.Login(member.getId(), member.getPwd());
+
+        if (!sql.isEmpty()) {
+            System.out.println("exist");
+            check = true;
+        } else {
+            System.out.println("fail");
+        }
+        return check;
     }
 }
