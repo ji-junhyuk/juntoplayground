5d1a6c69f76674c616cc684d5b0799dc7a7644f4
junto <takeoutmoneywallet@naver.com>
2020-12-06 05:01:30 +0900

    feat: Enroll member

--- YHIntro/src/main/java/spring/YHIntro/web/MemberController.java
+++ YHIntro/src/main/java/spring/YHIntro/web/MemberController.java
@@ -3,6 +3,8 @@ package spring.YHIntro.web;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import spring.YHIntro.domain.Member;
 import spring.YHIntro.service.MemberService;
 
 @Controller
@@ -14,5 +16,16 @@ public class MemberController {
     @GetMapping(value = "/members/new")
     public String createForm() {
         return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String create(MemberForm form) {
+
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
