0346d3bcd1cd0f0e1c264543991c04894533aacb
junto <takeoutmoneywallet@naver.com>
2020-12-06 04:59:43 +0900

    feat: Add createMemberForm

--- YHIntro/src/main/java/spring/YHIntro/web/MemberController.java
+++ YHIntro/src/main/java/spring/YHIntro/web/MemberController.java
@@ -2,6 +2,7 @@ package spring.YHIntro.web;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import spring.YHIntro.service.MemberService;
 
 @Controller
@@ -9,4 +10,9 @@ import spring.YHIntro.service.MemberService;
 public class MemberController {
 
     private final MemberService memberService;
+
+    @GetMapping(value = "/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
 }
