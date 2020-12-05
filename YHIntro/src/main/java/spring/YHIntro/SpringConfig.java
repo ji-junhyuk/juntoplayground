06efccb26a5ac3929b978463f0f6b3017f64c822
junto <takeoutmoneywallet@naver.com>
2020-12-06 05:25:19 +0900

    feat: Convert JDBC Template to JPA

--- YHIntro/src/main/java/spring/YHIntro/SpringConfig.java
+++ YHIntro/src/main/java/spring/YHIntro/SpringConfig.java
@@ -1,22 +1,23 @@
 package spring.YHIntro;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import spring.YHIntro.repository.JdbcTemplateMemberRepository;
+import spring.YHIntro.repository.JpaMemberRepository;
 import spring.YHIntro.repository.MemberRepository;
 import spring.YHIntro.repository.MemoryMemberRepository;
 import spring.YHIntro.service.MemberService;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
+@RequiredArgsConstructor
 public class SpringConfig {
 
     private final DataSource dataSource;
-
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+    private final EntityManager em;
 
     @Bean
     public MemberService memberService() {
@@ -26,6 +27,8 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
-        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
+
 }
