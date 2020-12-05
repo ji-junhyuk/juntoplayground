814881531ba3bd96114b6f9c5b266088484bc9e6
junto <takeoutmoneywallet@naver.com>
2020-12-05 09:31:15 +0900

    feat: Add join, findMembers, findOne

---
+++ YHJpa/src/main/java/spring/YHJpa/service/MemberService.java
@@ -0,0 +1,45 @@
+package spring.YHJpa.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import spring.YHJpa.domain.Member;
+import spring.YHJpa.repository.MemberRepository;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class MemberService {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    /*
+    Sign up
+     */
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMember = memberRepository.findByName(member.getName());
+        if (!findMember.isEmpty()) {
+            throw new IllegalStateException("Already existing member.");
+        }
+    }
+
+    /*
+    View all member
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
