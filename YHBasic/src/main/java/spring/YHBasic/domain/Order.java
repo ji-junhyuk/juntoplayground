5990be0f664120f2b06806e79380318b4de00d30
junto <takeoutmoneywallet@naver.com>
2020-12-07 04:59:31 +0900

    test: Test a OrderService(Junit5)

--- YHBasic/src/main/java/spring/YHBasic/domain/Order.java
+++ YHBasic/src/main/java/spring/YHBasic/domain/Order.java
@@ -1,5 +1,9 @@
 package spring.YHBasic.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class Order {
 
     private Long memberId;
---
+++ YHBasic/src/test/java/spring/YHBasic/service/OrderServiceTest.java
@@ -0,0 +1,31 @@
+package spring.YHBasic.service;
+
+import org.junit.jupiter.api.Test;
+import spring.YHBasic.domain.Grade;
+import spring.YHBasic.domain.Member;
+import spring.YHBasic.domain.Order;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    public void createOrder() {
+
+        //given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        //when
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+
+        //then
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
+}
