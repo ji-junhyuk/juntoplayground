3778ac183021f28fb93e9f884058d7062e989a60
junto <takeoutmoneywallet@naver.com>
2020-12-07 04:31:40 +0900

    feat: Implement MemberRepository(MMR)

---
+++ YHBasic/src/main/java/spring/YHBasic/repository/MemoryMemberRepository.java
@@ -0,0 +1,21 @@
+package spring.YHBasic.repository;
+
+import spring.YHBasic.domain.Member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public Member save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+}
