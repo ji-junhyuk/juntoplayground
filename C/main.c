f6ac2a3b54333cc8ce084877c231fca1a69bb601
junto <takeoutmoneywallet@naver.com>
2020-12-06 18:11:39 +0900

    C practice

--- C/main.c
+++ C/main.c
@@ -1,46 +1,13 @@
 #include <stdio.h>
 
-int main()
-{
-    int score;
-    char grade;
-    printf("enter the score:");
-    scanf("%d", &score);
+int main() {
+    for (int i = 65; i < 128; ++i) {
+        printf("%c", i);
 
-    switch (score/10) {
-        case 10:
-        case 9:
-            printf("grade: 'A'\n");
-            printf("Your %d score is %c credit.", score, grade);
-            break;
-        case 8:
-            printf("grade: B\n");
-            break;
-        case 7:
-            printf("grade: C\n");
-            break;
-        default:
-            printf("grade: D");
+
+        if (i % 8 == 0)
+            printf("\n");
+        continue;
+
     }
-
-
-/*   int score;
-   char grade;
-
-    printf("enter the score:");
-    scanf("%d", &score);
-
-    if (score >= 90) {
-        grade = 'A';
-    } else if (score >= 80) {
-        grade = 'B';
-    } else if (score >= 70) {
-        grade = 'C';
-    } else {
-        grade = 'D';
-    }
-
-    printf("Your %d score is %c credit.", score, grade);
-
-    return 0;*/
 }
