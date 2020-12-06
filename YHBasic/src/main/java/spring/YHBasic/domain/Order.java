a852c83fb00e29bbd9e55a9d2d307f165bdc99af
junto <takeoutmoneywallet@naver.com>
2020-12-07 04:46:20 +0900

    feat: Set a Order

---
+++ YHBasic/src/main/java/spring/YHBasic/domain/Order.java
@@ -0,0 +1,30 @@
+package spring.YHBasic.domain;
+
+public class Order {
+
+    private Long memberId;
+    private String itemName;
+    private int itemPrice;
+    private int discountPrice;
+
+    public Order(Long memberId, String itemName, int itemPrice, int discountPrice) {
+        this.memberId = memberId;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.discountPrice = discountPrice;
+    }
+
+    private int calculatePrice() {
+        return itemPrice - discountPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "memberId=" + memberId +
+                ", itemName='" + itemName + '\'' +
+                ", itemPrice=" + itemPrice +
+                ", discountPrice=" + discountPrice +
+                '}';
+    }
+}
