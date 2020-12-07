442cb408294333234822908adbfa8f9f45bcbe20
junto <takeoutmoneywallet@naver.com>
2020-12-08 05:54:57 +0900

    feat: Set a OrderRepository

---
+++ YHJpa/src/main/java/spring/YHJpa/repository/OrderRepository.java
@@ -0,0 +1,22 @@
+package spring.YHJpa.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import spring.YHJpa.domain.Order;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+}
