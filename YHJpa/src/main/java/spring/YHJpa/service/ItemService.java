34e4860ff13464688e88e7264ad9ea14641150c4
junto <takeoutmoneywallet@naver.com>
2020-12-08 05:42:16 +0900

    feat: Set a ItemRepository

---
+++ YHJpa/src/main/java/spring/YHJpa/service/ItemService.java
@@ -0,0 +1,30 @@
+package spring.YHJpa.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import spring.YHJpa.domain.item.Item;
+import spring.YHJpa.repository.ItemRepository;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    private Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+}
