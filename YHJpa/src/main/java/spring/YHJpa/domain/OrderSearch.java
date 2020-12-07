c4b8377a7f7fddb3db0d69d81cf6cbe51f150fde
junto <takeoutmoneywallet@naver.com>
2020-12-08 05:56:12 +0900

    feat: Set a OrderSearch

---
+++ YHJpa/src/main/java/spring/YHJpa/domain/OrderSearch.java
@@ -0,0 +1,13 @@
+package spring.YHJpa.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderSearch {
+
+    private String memberName;
+
+    private OrderStatus orderStatus;
+}
