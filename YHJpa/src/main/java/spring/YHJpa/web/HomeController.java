3b5ef2add71de32bc03d343ebf7ef78e78f68119
junto <takeoutmoneywallet@naver.com>
2020-12-08 08:55:59 +0900

    refactor: Rename HomeController(Member)

--- YHJpa/src/main/java/spring/YHJpa/web/HomeController.java
+++ YHJpa/src/main/java/spring/YHJpa/web/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
-public class MemberController {
+public class HomeController {
 
     @RequestMapping("/")
     public String home() {
