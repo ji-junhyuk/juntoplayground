60cd2d653fad532970c3b76e13a8a3ed968543cd
junto <takeoutmoneywallet@naver.com>
2020-12-08 04:54:35 +0900

    feat: Set a Category

--- YHJpa/src/main/java/spring/YHJpa/domain/item/Category.java
+++ YHJpa/src/main/java/spring/YHJpa/domain/item/Category.java
@@ -1,4 +1,29 @@
 package spring.YHJpa.domain.item;
 
+import lombok.Getter;
+import lombok.Setter;
+import spring.YHJpa.domain.Address;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
 public class Category {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    private List<Item> items = new ArrayList<>();
+
+    private Address parent;
+
+    private Address child;
 }
--- YHJpa/src/main/java/spring/YHJpa/domain/item/Item.java
+++ YHJpa/src/main/java/spring/YHJpa/domain/item/Item.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
 @Getter
 @Setter
 public class Item {
