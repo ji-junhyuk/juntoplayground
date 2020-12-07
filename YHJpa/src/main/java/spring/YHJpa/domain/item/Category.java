4c9c011bed3d4d7fbbdab9c80d1853d2ea9dccbf
junto <takeoutmoneywallet@naver.com>
2020-12-08 05:13:01 +0900

    refactor: Modify Category

--- YHJpa/src/main/java/spring/YHJpa/domain/item/Category.java
+++ YHJpa/src/main/java/spring/YHJpa/domain/item/Category.java
@@ -21,9 +21,21 @@ public class Category {
     private String name;
 
     @ManyToMany
+    @JoinTable(name = "category_item",
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id"))
     private List<Item> items = new ArrayList<>();
 
-    private Address parent;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
 
-    private Address child;
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+    //==Association Method==//
+    public void addChildCategory(Category child) {
+        this.child.add(child);
+        child.setParent(this);
+    }
 }
