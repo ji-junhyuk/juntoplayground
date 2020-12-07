e4b5e8019ec241567396df513c9b4c5f7c3c1c62
junto <takeoutmoneywallet@naver.com>
2020-12-08 05:08:38 +0900

    refactor: Modify Movie, Book, Album

--- YHJpa/src/main/java/spring/YHJpa/domain/item/Book.java
+++ YHJpa/src/main/java/spring/YHJpa/domain/item/Book.java
@@ -3,9 +3,11 @@ package spring.YHJpa.domain.item;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
+@DiscriminatorValue("book")
 @Getter
 @Setter
 public class Book extends Item {
