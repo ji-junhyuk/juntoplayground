136b4208462ced0d15e49da6b2ea37d2c46551f8
junto <takeoutmoneywallet@naver.com>
2020-12-08 04:29:21 +0900

    C Practice

--- C/main.c
+++ C/main.c
@@ -2,8 +2,41 @@
 
 int main()
 {
+    int year, mon, lastday = 0;
 
-    for (int i = 1; i <= 10; ++i) {
-        printf("tree %d\n", i);
+    printf("Enter the year: ");
+    scanf("%d", &year);
+    printf("Enter the mon:");
+    scanf("%d", &mon);
+
+    switch (mon)
+    {
+        case 1:case 3: case 5: case 7:case 8:case 10:case 12:
+            lastday = 31;
+            break;
+
+        case 4: case 6: case 9: case 11:
+            lastday = 30;
+            break;
+
+        case 2:
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+            {
+                lastday = 29;
+            }
+            else
+            {
+                lastday = 28;
+            }
+            break;
+
+        default:
+            printf("wrong mon.");
+
     }
+
+    (1 <= mon && mon <= 12) ? printf("%d year %d mon last day is %d.\n", year, mon, lastday) : printf("\n");
+
+    return 0;
 }
+
