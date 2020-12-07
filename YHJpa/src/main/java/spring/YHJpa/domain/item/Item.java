099a601375bb07ca1370ba083f32e1cb453d0834
junto <takeoutmoneywallet@naver.com>
2020-12-08 04:52:16 +0900

    feat: Set a Item

--- YHJpa/src/main/java/spring/YHJpa/domain/item/Item.java
+++ YHJpa/src/main/java/spring/YHJpa/domain/item/Item.java
@@ -1,4 +1,29 @@
 package spring.YHJpa.domain.item;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
 public class Item {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+
+    private int price;
+
+    private int stockQuantity;
+
+    @ManyToMany
+    private List<Category> categories = new ArrayList<>();
+
 }
