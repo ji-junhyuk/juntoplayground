d88c9493790f0c43ea0cafb3e6f8566b7c7aefc8
junto <takeoutmoneywallet@naver.com>
2020-12-10 04:43:35 +0900

    feat: Use RestController

---
+++ HH/src/main/java/spring/HH/controller/HelloController.java
@@ -0,0 +1,14 @@
+package spring.HH.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping(value = "/api/helloWorld")
+    public String HelloWorld() {
+        return "HelloWorld";
+    }
+}
