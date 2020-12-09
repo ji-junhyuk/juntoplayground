64613d305d11f9fae22358ed1cfa34b46eb2b7eb
junto <takeoutmoneywallet@naver.com>
2020-12-09 12:37:25 +0900

    feat: Set a Java Spring Web Development

---
+++ HH/src/main/java/spring/HH/HhApplication.java
@@ -0,0 +1,13 @@
+package spring.HH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhApplication.class, args);
+	}
+
+}
