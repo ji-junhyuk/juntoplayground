174c131d7e73905fc90e0ee096eae39347ee4160
junto <takeoutmoneywallet@naver.com>
2020-12-09 12:32:07 +0900

    feat: Set a Java Spring Web Development

---
+++ HH/src/test/java/spring/HH/HhApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.HH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
