3529add85b69c1e612293c23d1bf5f007ac9382a
junto <takeoutmoneywallet@naver.com>
2020-12-12 04:18:05 +0900

    feat: Convert annotation to java code

--- YHIntro/src/main/java/spring/YHIntro/service/MemberService.java
+++ YHIntro/src/main/java/spring/YHIntro/service/MemberService.java
@@ -9,12 +9,10 @@ import spring.YHIntro.repository.MemoryMemberRepository;
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
-    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
