10f0292e3073c4ef83fa53e8375ea2b57f6a1158
junto <takeoutmoneywallet@naver.com>
2020-12-12 03:37:42 +0900

    feat: Use data annotation

--- HH/src/main/java/spring/HH/domain/Person.java
+++ HH/src/main/java/spring/HH/domain/Person.java
@@ -1,18 +1,23 @@
 package spring.HH.domain;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
 @Getter
 @Setter
 @ToString
-
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@EqualsAndHashCode
+@Data
 public class Person {
 
     @Id
@@ -20,7 +25,13 @@ public class Person {
     @ToString.Exclude
     private Long id;
 
+    @NonNull
     private String name;
 
+    @NonNull
     private int age;
+
+    private String hobby;
+
+    private LocalDate birthday;
 }
--- HH/src/test/java/spring/HH/repository/PersonRepositoryTest.java
+++ HH/src/test/java/spring/HH/repository/PersonRepositoryTest.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import spring.HH.domain.Person;
 
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -33,5 +36,27 @@ class PersonRepositoryTest {
         assertThat(people.size()).isEqualTo(1);
         assertThat(people.get(0).getName()).isEqualTo("martin");
         assertThat(people.get(0).getAge()).isEqualTo(10);
+    }
+
+    @Test
+    void constructorTest() {
+        Person person = new Person("martin", 10);
+//        Person person = new Person(1L, "martin", 10, "Soccer", LocalDate.of(1994, 03, 03));
+    }
+
+    @Test
+    void hashCodeAndEquals() {
+        Person person1 = new Person("martin", 10);
+        Person person2 = new Person("martin", 10);
+
+        System.out.println(person1.equals(person2));
+        System.out.println(person1.hashCode());
+        System.out.println(person2.hashCode());
+
+        Map<Person, Integer> map = new HashMap<>();
+        map.put(person1, person1.getAge());
+        System.out.println("**********");
+        System.out.println(map);
+        System.out.println(map.get(person2));
     }
 }
