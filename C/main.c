40fbbf7c2fb46fbd18c7259d345a2c46638e30ac
junto <takeoutmoneywallet@naver.com>
2020-12-12 03:36:45 +0900

    C practice

--- C/main.c
+++ C/main.c
@@ -1,13 +1,33 @@
-#include <string.h>
 #include <stdio.h>
 
+int med3(int a, int b, int c)
+{
+    if ((b>=a && c<=a) || (b < a && c >= a))
+        return a;
+    else if ((a > b && c < b) || (a < b && c > b)) {
+        return b;
+    }
+    
+    return c;
+}
 int main()
 {
-    char fruit[30] = "apple";
-    int len;
+    printf("%d %d %d = median: %d\n", 3, 3, 3, med3(3, 3, 3));// a=b=c
+    printf("%d %d %d = median: %d\n", 3, 2, 1, med3(3, 2, 1));// b > c
+    printf("%d %d %d = median: %d\n", 3, 3, 2, med3(3, 3, 2)); // (a = b)
+    printf("%d %d %d = median: %d\n", 3, 1, 2, med3(3, 1, 2));// b < c
+    printf("%d %d %d = median: %d\n", 3, 2, 3, med3(3, 2, 3)); // (a = c),
+    printf("%d %d %d = median: %d\n", 3, 2, 2, med3(3, 2, 2));// b = c
 
-    len = strlen(fruit);
+    printf("%d %d %d = median: %d\n", 2, 3, 1, med3(2, 3, 1));// a > c
+    printf("%d %d %d = median: %d\n", 2, 3, 2, med3(2, 3, 2)); // (a = c)
+    printf("%d %d %d = median: %d\n", 1, 3, 2, med3(1, 3, 2));// a < c
+    printf("%d %d %d = median: %d\n", 2, 3, 3, med3(2, 3, 3));// b = c
 
-    printf("fruit[] length : %d", len);
+    printf("%d %d %d = median: %d\n", 2, 1, 3, med3(2, 1, 3)); // a > b
+    printf("%d %d %d = median: %d\n", 1, 2, 3, med3(1, 2, 3)); // a < b
+    printf("%d %d %d = median: %d\n", 2, 2, 3, med3(2, 2, 3)); // a == b
+
+    return 0;
 }
 
