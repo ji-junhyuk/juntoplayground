9cf63465be647c8336af06bd2bee581a508f614e
junto <takeoutmoneywallet@naver.com>
2020-12-12 04:08:18 +0900

    feat: Set a MemberService

---
+++ YHIntro/src/main/java/spring/YHIntro/service/MemberService.java
@@ -0,0 +1,35 @@
+package spring.YHIntro.service;
+
+import spring.YHIntro.domain.Member;
+import spring.YHIntro.repository.MemberRepository;
+import spring.YHIntro.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public Long join(Member member) {
+
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("Already existing member.");
+                });
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+}
