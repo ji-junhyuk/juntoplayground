257e4a061253fafdb2d064cb324d02ef22880b63
junto <takeoutmoneywallet@naver.com>
2020-12-13 05:29:49 +0900

    feat: Set a FixDiscountPolicy

---
+++ YHBasic/src/main/java/spring/YHBasic/discount/FixDiscountPolicy.java
@@ -0,0 +1,19 @@
+package spring.YHBasic.discount;
+
+import spring.YHBasic.domain.Grade;
+import spring.YHBasic.domain.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+
+    private int discountFixAmount = 1000;
+
+    @Override
+    public int discount(Member member, int price) {
+
+        if (member.getGrade() == Grade.VIP) {
+            return discountFixAmount;
+        } else {
+            return 0;
+        } 
+    }
+}
