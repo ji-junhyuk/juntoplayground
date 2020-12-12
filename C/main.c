5f4b88a18b305a3e60a5ee4d7632ecce591a84f5
junto <takeoutmoneywallet@naver.com>
2020-12-13 04:39:10 +0900

    C Practice

--- C/main.c
+++ C/main.c
@@ -1,33 +1,17 @@
 #include <stdio.h>
 
-int med3(int a, int b, int c)
+int main(void)
 {
-    if ((b>=a && c<=a) || (b < a && c >= a))
-        return a;
-    else if ((a > b && c < b) || (a < b && c > b)) {
-        return b;
+    int i, j, n;
+    do {
+        printf("What floor triangle: ");
+        scanf("%d", &n);
+    } while (n <= 0);
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            printf("*");
+        }
+        printf("\n");
     }
-    
-    return c;
 }
-int main()
-{
-    printf("%d %d %d = median: %d\n", 3, 3, 3, med3(3, 3, 3));// a=b=c
-    printf("%d %d %d = median: %d\n", 3, 2, 1, med3(3, 2, 1));// b > c
-    printf("%d %d %d = median: %d\n", 3, 3, 2, med3(3, 3, 2)); // (a = b)
-    printf("%d %d %d = median: %d\n", 3, 1, 2, med3(3, 1, 2));// b < c
-    printf("%d %d %d = median: %d\n", 3, 2, 3, med3(3, 2, 3)); // (a = c),
-    printf("%d %d %d = median: %d\n", 3, 2, 2, med3(3, 2, 2));// b = c
-
-    printf("%d %d %d = median: %d\n", 2, 3, 1, med3(2, 3, 1));// a > c
-    printf("%d %d %d = median: %d\n", 2, 3, 2, med3(2, 3, 2)); // (a = c)
-    printf("%d %d %d = median: %d\n", 1, 3, 2, med3(1, 3, 2));// a < c
-    printf("%d %d %d = median: %d\n", 2, 3, 3, med3(2, 3, 3));// b = c
-
-    printf("%d %d %d = median: %d\n", 2, 1, 3, med3(2, 1, 3)); // a > b
-    printf("%d %d %d = median: %d\n", 1, 2, 3, med3(1, 2, 3)); // a < b
-    printf("%d %d %d = median: %d\n", 2, 2, 3, med3(2, 2, 3)); // a == b
-
-    return 0;
-}
-
