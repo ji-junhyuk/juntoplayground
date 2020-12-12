2fd96dd90ad62544f7eff0bc28f6cdf61c5c7c5f
junto <takeoutmoneywallet@naver.com>
2020-12-13 05:13:27 +0900

    test: Use MemberApp

---
+++ YHBasic/src/main/java/spring/YHBasic/MemberApp.java
@@ -0,0 +1,19 @@
+package spring.YHBasic;
+
+import spring.YHBasic.domain.Grade;
+import spring.YHBasic.domain.Member;
+import spring.YHBasic.service.MemberService;
+import spring.YHBasic.service.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find member = " + findMember.getName());
+    }
+}
