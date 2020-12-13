12e48d4f2a7d648fb9307800e8c03460a8c488b6
junto <takeoutmoneywallet@naver.com>
2020-12-14 05:29:14 +0900

    C Practice

--- C/main.c
+++ C/main.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 
+void nrpira(int n)
+{
+	int i, j;
+
+	for (i = 1; i <= n; i++) {							/* i 행 (i = 1, 2, ..., n) */
+		for (j = 1; j <= i - 1; j++)					/* i-1 개의 ' '을 출력 */
+			putchar(' ');
+		for (j = 1; j <= (n - i) * 2 + 1; j++)			/* (n-i) * 2 + 1개의 숫자를 출력 */
+			printf("%d", i % 10);
+		putchar('\n');
+	}
+}
+
 int main(void)
 {
-    int i, j, n;
-    do {
-        printf("What floor triangle: ");
-        scanf("%d", &n);
-    } while (n <= 0);
+	int n;
 
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= i; ++j) {
-            printf("*");
-        }
-        printf("\n");
-    }
+	puts("Pyramid.");
+	do {
+		printf("\nfloor : ");
+		scanf("%d", &n);
+	} while (n <= 0);
+
+	nrpira(n);
+
+	return 0;
 }
