1ec6ad6223aecf8ecfd9578d309c137eb93a52eb
junto <takeoutmoneywallet@naver.com>
2020-12-15 06:28:51 +0900

    chore: Practice C

--- C/main.c
+++ C/main.c
@@ -1,29 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-void nrpira(int n)
-{
-	int i, j;
+int main() {
+    int x, y;
 
-	for (i = 1; i <= n; i++) {							/* i 행 (i = 1, 2, ..., n) */
-		for (j = 1; j <= i - 1; j++)					/* i-1 개의 ' '을 출력 */
-			putchar(' ');
-		for (j = 1; j <= (n - i) * 2 + 1; j++)			/* (n-i) * 2 + 1개의 숫자를 출력 */
-			printf("%d", i % 10);
-		putchar('\n');
-	}
-}
+    srand(time(NULL));
+    x = rand() % 6;
+    y = rand() % 6;
 
-int main(void)
-{
-	int n;
-
-	puts("Pyramid.");
-	do {
-		printf("\nfloor : ");
-		scanf("%d", &n);
-	} while (n <= 0);
-
-	nrpira(n);
-
-	return 0;
+    printf("%d, %d\n", x, y);
 }
