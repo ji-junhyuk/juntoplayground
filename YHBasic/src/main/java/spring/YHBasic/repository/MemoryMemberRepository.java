ea21859852092c8868260bea8ae231ccccac91ec
junto <takeoutmoneywallet@naver.com>
2020-12-20 04:34:22 +0900

    feat: Implement MemberRepository

---
+++ YHBasic/src/main/java/spring/YHBasic/repository/MemoryMemberRepository.java
@@ -0,0 +1,21 @@
+package spring.YHBasic.repository;
+
+import spring.YHBasic.domain.Member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
