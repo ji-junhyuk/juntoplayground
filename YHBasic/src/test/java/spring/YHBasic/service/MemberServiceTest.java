6323320164105a4ab974535ce8aa51d3a247aebd
junto <takeoutmoneywallet@naver.com>
2020-12-20 05:12:10 +0900

    refactor: Use Appconfig to MemberServiceTest

--- YHBasic/src/test/java/spring/YHBasic/service/MemberServiceTest.java
+++ YHBasic/src/test/java/spring/YHBasic/service/MemberServiceTest.java
@@ -1,13 +1,21 @@
 package spring.YHBasic.service;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import spring.YHBasic.AppConfig;
 import spring.YHBasic.domain.Grade;
 import spring.YHBasic.domain.Member;
 
 class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     public void join() {
