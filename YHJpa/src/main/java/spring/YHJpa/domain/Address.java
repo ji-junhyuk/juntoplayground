a8b6433d63e235b99bc915f899936b96bfbe3bbc
junto <takeoutmoneywallet@naver.com>
2020-12-21 05:01:26 +0900

    feat: Set a Address

--- YHJpa/src/main/java/spring/YHJpa/domain/Address.java
+++ YHJpa/src/main/java/spring/YHJpa/domain/Address.java
@@ -1,4 +1,17 @@
 package spring.YHJpa.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+@NoArgsConstructor @AllArgsConstructor
 public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
 }
