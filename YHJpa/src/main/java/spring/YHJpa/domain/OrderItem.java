b2ff88bdca760f9f4ac4b887002343530c98b8c1
junto <takeoutmoneywallet@naver.com>
2020-12-21 04:49:45 +0900

    feat: Set a OrderItem

--- YHJpa/src/main/java/spring/YHJpa/domain/OrderItem.java
+++ YHJpa/src/main/java/spring/YHJpa/domain/OrderItem.java
@@ -3,8 +3,22 @@ package spring.YHJpa.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-@Setter
+import javax.persistence.*;
+
+@Entity
+@Table(name = "order_item")
+@Getter @Setter
 public class OrderItem {
 
+    @Id
+    @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
     private Order order;
+
+    private int orderPrice;
+    private int count;
 }
