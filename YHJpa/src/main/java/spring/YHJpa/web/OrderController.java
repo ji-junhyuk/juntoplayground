d46388e35c267f1f12cf61df41659d141ee1a1e0
junto <takeoutmoneywallet@naver.com>
2020-12-21 06:23:47 +0900

    feat: Order item

---
+++ YHJpa/src/main/java/spring/YHJpa/web/OrderController.java
@@ -0,0 +1,45 @@
+package spring.YHJpa.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import spring.YHJpa.domain.Member;
+import spring.YHJpa.domain.item.Item;
+import spring.YHJpa.service.ItemService;
+import spring.YHJpa.service.MemberService;
+import spring.YHJpa.service.OrderService;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final MemberService memberService;
+    private final ItemService itemService;
+    private final OrderService orderService;
+
+    @GetMapping(value = "/order")
+    public String createForm(Model model) {
+
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping(value = "/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+}
