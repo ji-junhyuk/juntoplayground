d890057c147b095a8521a17a7272dee20b675a92
junto <takeoutmoneywallet@naver.com>
2020-12-21 05:27:15 +0900

    feat: Add CreateOrderItem in OrderItem

--- YHJpa/src/main/java/spring/YHJpa/domain/OrderItem.java
+++ YHJpa/src/main/java/spring/YHJpa/domain/OrderItem.java
@@ -2,6 +2,7 @@ package spring.YHJpa.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import spring.YHJpa.domain.item.Item;
 
 import javax.persistence.*;
 
@@ -16,9 +17,26 @@ public class OrderItem {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
 
     private int orderPrice;
     private int count;
+
+    //==Create Method==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+
+        return orderItem;
+    }
 }
