b6c9aa8b8b4bd2fffc53a6417162515c0e630909
junto <takeoutmoneywallet@naver.com>
2020-12-21 06:19:01 +0900

    feat: Modify Item

--- YHJpa/src/main/java/spring/YHJpa/service/ItemService.java
+++ YHJpa/src/main/java/spring/YHJpa/service/ItemService.java
@@ -27,4 +27,12 @@ public class ItemService {
     public List<Item> findItems() {
         return itemRepository.findAll();
     }
+
+    @Transactional
+    public void updateItem(Long id, String name, int price, int quantity) {
+        Item item = itemRepository.findOne(id);
+        item.setName(name);
+        item.setPrice(price);
+        item.setStockQuantity(quantity);
+    }
 }
