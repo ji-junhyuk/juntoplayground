39697e4b8230f150f5a57487dea8dbbc29004916
junto <takeoutmoneywallet@naver.com>
2020-12-21 05:16:40 +0900

    feat: Add Business Logic(Item)

--- YHJpa/src/main/java/spring/YHJpa/domain/item/Item.java
+++ YHJpa/src/main/java/spring/YHJpa/domain/item/Item.java
@@ -2,6 +2,7 @@ package spring.YHJpa.domain.item;
 
 import lombok.Getter;
 import lombok.Setter;
+import spring.YHJpa.exception.NotEnoughStockException;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -25,4 +26,17 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<Category>();
+
+    //==Business Logic==//
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity = quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
