22b23eb41830ad6355b9c7106edd793dbbb497d1
junto <takeoutmoneywallet@naver.com>
2020-12-21 09:16:25 +0900

    feat: Set a Block

---
+++ HH/src/main/java/spring/HH/domain/Block.java
@@ -0,0 +1,30 @@
+package spring.HH.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Block {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NonNull
+    private String name;
+
+    private String reason;
+
+    private LocalDate startDate;
+    private LocalDate EndDate;
+}
