5b1f47b3e904100628231d64d8ecc86bcfe8948f
junto <takeoutmoneywallet@naver.com>
2020-12-25 05:11:18 +0900

    feat: Add JsonSerializer

---
+++ HH/src/main/java/spring/HH/configuration/serializer/BirthdaySerializer.java
@@ -0,0 +1,18 @@
+package spring.HH.configuration.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+public class BirthdaySerializer extends JsonSerializer {
+
+    @Override
+    public void serialize(Object value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        if (value != null) {
+            gen.writeObject(LocalDate.of(value.getYearOfBirthday(), value.getMonthOfBirthday(), value.getDayOfBirthday()));
+        }
+    }
+}
