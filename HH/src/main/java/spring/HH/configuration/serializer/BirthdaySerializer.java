ead3627d29ca56385639ebfbfba954e1c5fe994c
junto <takeoutmoneywallet@naver.com>
2020-12-25 06:28:32 +0900

    feat: Modify BirthdaySerializer add <Birthday>

--- HH/src/main/java/spring/HH/configuration/serializer/BirthdaySerializer.java
+++ HH/src/main/java/spring/HH/configuration/serializer/BirthdaySerializer.java
@@ -3,14 +3,16 @@ package spring.HH.configuration.serializer;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import spring.HH.domain.dto.Birthday;
 
 import java.io.IOException;
 import java.time.LocalDate;
 
-public class BirthdaySerializer extends JsonSerializer {
+public class BirthdaySerializer extends JsonSerializer<Birthday> {
 
     @Override
-    public void serialize(Object value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+    public void serialize(Birthday value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
         if (value != null) {
             gen.writeObject(LocalDate.of(value.getYearOfBirthday(), value.getMonthOfBirthday(), value.getDayOfBirthday()));
         }
