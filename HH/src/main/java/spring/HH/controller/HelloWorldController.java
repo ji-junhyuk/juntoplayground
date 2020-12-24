821ede9542b2e2bfd8f562c9265e916b66dd3ea3
junto <takeoutmoneywallet@naver.com>
2020-12-25 05:26:52 +0900

    feat: Set a HelloWorldController

---
+++ HH/src/main/java/spring/HH/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package spring.HH.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(value = "/api/helloWorld")
+    public String helloWorld() {
+        return "HelloWorld";
+    }
+
+    @GetMapping(value = "/api/helloException")
+    public String helloException() {
+        throw new RuntimeException("Hello RuntimeException");
+    }
+}
