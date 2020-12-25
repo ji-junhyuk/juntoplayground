feace9771d78aba89fe6646e234f03629deaf22a
junto <takeoutmoneywallet@naver.com>
2020-12-25 15:45:34 +0900

    feat: Use bubble sort version 2(View Exchange, comparison)

--- C/algorithm/q6-3.c
+++ C/algorithm/q6-3.c
@@ -5,40 +5,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define swap(type, x, y) do { type t = x; x = y; y = t; } while (0)
+#define swap(type, x, y)  do { type t = x; x = y; y = t; } while (0)
 
+/*--- 버블 정렬 응용 ---*/
 int is_sorted(int a[], int n)
 {
-    for (int i = n - 1; i > 0; --i) {
-        if (a[i - 1] > a[i])
-            return 0;
-    }
-    return 1;
+	int i;
+
+	for (i = n - 1; i > 0; i--)
+		if (a[i - 1] > a[i])
+			return 0;
+	return 1;
 }
 
 int main(void)
 {
-    int i, nx, sorted;
-    int *x;
+	int i, nx, sorted;
+	int *x;
 
-    puts("bublle sort deepening");
-    printf("number of element:");
-    scanf("%d", &nx);
-    x = calloc(nx, sizeof(int));
+	puts("버블 정렬 응용(배열 정렬 검사)");
+	printf("요솟수 : ");
+	scanf("%d", &nx);
+	x = calloc(nx, sizeof(int));
 
-    for (int i = 0; i < nx; ++i) {
-        printf("x[%d]: ", i);
-        scanf("%d", &x[i]);
-    }
+	for (i = 0; i < nx; i++) {
+		printf("x[%d] : ", i);
+		scanf("%d", &x[i]);
+	}
 
-    sorted = is_sorted(x, nx);
+	sorted = is_sorted(x, nx);		/* 배열 x가 정렬되었는지 확인 */
 
-    if (sorted)
-        puts("array is arranged.");
-    else
-        puts("array is not arranged.");
+	if (sorted)
+		puts("배열이 정렬되어 있습니다.");
+	else
+		puts("배열이 정렬되어있지 않습니다.");
 
-    free(x);
+	free(x);
 
-    return 0;
+	return 0;
 }
