1ae4c6043515299d8449c67fa0a1734200c38c93
junto <takeoutmoneywallet@naver.com>
2020-12-26 05:19:27 +0900

    feat: Set a JdbcTemplateMemberRepository

--- YHIntro/src/main/java/spring/YHIntro/SpringConfig.java
+++ YHIntro/src/main/java/spring/YHIntro/SpringConfig.java
@@ -1,13 +1,24 @@
 package spring.YHIntro;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import spring.YHIntro.repository.JdbcTemplateMemberRepository;
 import spring.YHIntro.repository.MemberRepository;
 import spring.YHIntro.repository.MemoryMemberRepository;
 import spring.YHIntro.service.MemberService;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
