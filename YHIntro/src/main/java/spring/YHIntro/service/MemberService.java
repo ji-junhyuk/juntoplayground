426267e0fb71896fc662e2b749a10cc0523b4f0e
junto <takeoutmoneywallet@naver.com>
2020-12-26 04:59:17 +0900

    feat: Use component scan

--- YHIntro/src/main/java/spring/YHIntro/service/MemberService.java
+++ YHIntro/src/main/java/spring/YHIntro/service/MemberService.java
@@ -1,6 +1,7 @@
 package spring.YHIntro.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import spring.YHIntro.domain.Member;
 import spring.YHIntro.repository.MemberRepository;
 import spring.YHIntro.repository.MemoryMemberRepository;
@@ -8,6 +9,7 @@ import spring.YHIntro.repository.MemoryMemberRepository;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 @RequiredArgsConstructor
 public class MemberService {
 
