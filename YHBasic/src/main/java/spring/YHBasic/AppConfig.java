73fae10601212f5c290c6ace3a6233972045e296
junto <takeoutmoneywallet@naver.com>
2020-12-27 05:48:48 +0900

    refactor: Modify AppConfig more visible

--- YHBasic/src/main/java/spring/YHBasic/AppConfig.java
+++ YHBasic/src/main/java/spring/YHBasic/AppConfig.java
@@ -1,6 +1,9 @@
 package spring.YHBasic;
 
+import spring.YHBasic.discount.DiscountPolicy;
 import spring.YHBasic.discount.FixDiscountPolicy;
+import spring.YHBasic.discount.RateDiscountPolicy;
+import spring.YHBasic.repository.MemberRepository;
 import spring.YHBasic.repository.MemoryMemberRepository;
 import spring.YHBasic.service.MemberService;
 import spring.YHBasic.service.MemberServiceImpl;
@@ -10,13 +13,22 @@ import spring.YHBasic.service.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
         return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy()
+                memberRepository(),
+                discountPolicy()
         );
+    }
+
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
