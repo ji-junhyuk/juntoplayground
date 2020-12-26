079ab3d4e7c3dfa5fd594f3422f34ced6759cedc
junto <takeoutmoneywallet@naver.com>
2020-12-27 05:52:31 +0900

    test: Set a OrderApp

---
+++ YHBasic/src/main/java/spring/YHBasic/OrderApp.java
@@ -0,0 +1,23 @@
+package spring.YHBasic;
+
+import spring.YHBasic.domain.Grade;
+import spring.YHBasic.domain.Member;
+import spring.YHBasic.domain.Order;
+import spring.YHBasic.service.MemberService;
+import spring.YHBasic.service.OrderService;
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
+
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+
+        System.out.println("order = " + order);
+    }
+}
