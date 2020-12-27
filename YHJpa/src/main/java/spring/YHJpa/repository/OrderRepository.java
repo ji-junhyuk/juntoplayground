9399aeaf675e36c6b25bbcfec592bf9110c3ba66
junto <takeoutmoneywallet@naver.com>
2020-12-28 05:45:42 +0900

    feat: Set a OrderSearch

---
+++ YHJpa/src/main/java/spring/YHJpa/repository/OrderRepository.java
@@ -0,0 +1,34 @@
+package spring.YHJpa.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import spring.YHJpa.domain.Order;
+import spring.YHJpa.domain.OrderSearch;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+    public List<Order> findAllByString(OrderSearch orderSearch) {
+
+        String jpql = "select o From Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        if (orderSearch.getOrderStatus() != null) {
+
+        }
+    }
+}
