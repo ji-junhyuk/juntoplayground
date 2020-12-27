af166f18ca2b3a860d8ac0d86291b5e185c76880
junto <takeoutmoneywallet@naver.com>
2020-12-28 05:04:48 +0900

    feat: Set a MemberService

---
+++ YHJpa/src/main/java/spring/YHJpa/service/MemberService.java
@@ -0,0 +1,40 @@
+package spring.YHJpa.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import spring.YHJpa.domain.Member;
+import spring.YHJpa.repository.MemberRepository;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class MemberService {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Transactional
+    public Long join(Member member) {
+
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("Already existing member.");
+        }
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+}
