89cda95ebfaba23e9656fdc7e007ec6cfe7011a4
junto <takeoutmoneywallet@naver.com>
2021-01-04 14:31:26 +0900

    feat: Use function pointer

---
+++ C/algorithm/ch3/kuku.c
@@ -0,0 +1,33 @@
+//
+// Created by junto on 21. 1. 4..
+//
+
+#include <stdio.h>
+
+int sum(int x1, int x2)
+{
+    return x1 + x2;
+}
+
+int mul(int x1, int x2)
+{
+    return x1 * x2;
+}
+
+void kuku(int (*calc)(int, int)) // void kuku(int calc(int, int))
+{
+    int i, j;
+    for (i = 1; i < 10; i++) {
+        for (j = 1; j < 10; j++)
+            printf("%3d", (*calc)(i, j)); // calc(i,j)
+        putchar('\n');
+    }
+}
+
+int main(void)
+{
+    puts("Addition table");
+    kuku(sum);
+    puts("\nMultiplication table");
+    kuku(mul);
+}
