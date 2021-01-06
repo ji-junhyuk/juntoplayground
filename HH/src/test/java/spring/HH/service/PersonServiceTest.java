7d1b847ff2a48bfb6a004533adad18a7d70a2313
junto <takeoutmoneywallet@naver.com>
2021-01-06 12:56:28 +0900

    feat: Study HH Lecture (25) 27.5/35

--- HH/src/test/java/spring/HH/service/PersonServiceTest.java
+++ HH/src/test/java/spring/HH/service/PersonServiceTest.java
@@ -1,57 +1,119 @@
 package spring.HH.service;
 
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import spring.HH.controller.dto.PersonDto;
 import spring.HH.domain.Person;
 import spring.HH.repository.PersonRepository;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 @Transactional
 class PersonServiceTest {
 
-    @Autowired
+    @InjectMocks
     private PersonService personService;
-    @Autowired
+    @Mock
     private PersonRepository personRepository;
 
     @Test
     void getPeoPleByName() {
 
-        //Given
-        givenPeoPle();
+        when(personRepository.findByName("junhyuk"))
+                .thenReturn(Lists.newArrayList(new Person("junhyuk")));
 
-        //When
-        List<Person> result = personService.getPeopleByName("dennis");
+        List<Person> result = personService.getPeopleByName("junhyuk");
 
-        //Then
         assertThat(result.size()).isEqualTo(1);
+        assertThat(result.get(0).getName()).isEqualTo("junhyuk");
     }
 
     @Test
     void getPerson() {
+        when(personRepository.findById(1L))
+                .thenReturn(Optional.of(new Person("junhyuk")));
+
+        Person person = personService.getPerson(1L);
+
+        assertThat(person.getName()).isEqualTo("junhyuk");
+    }
+
+    @Test
+    void getPersonIfNotFound() {
 
         //Given
-        givenPeoPle();
+        when(personRepository.findById(1L))
+                .thenReturn(Optional.empty());
 
-        //WhenExcludeBlock
-        Person result = personService.getPerson(1L);
+        //When
+        Person person = personService.getPerson(1L);
 
         //Then
-        assertThat(result.getName()).isEqualTo("junhyuk");
+        assertThat(person).isNull();
     }
 
-    private void givenPeoPle() {
-        givenPerson("junhyuk");
-        givenPerson("david");
+    @Test
+    void put() {
+
+        //When
+        personService.put(mockPersonDto());
+
+        //Then
+        verify(personRepository, times(1)).save(any(Person.class));
     }
 
-    private void givenPerson(String name) {
-        personRepository.save(new Person(name));
+    @Test
+    void modifyIfPersonNotFound() {
+
+        //Given
+        when(personRepository.findById(1L))
+                .thenReturn(Optional.empty());
+
+        //Then
+        assertThrows(RuntimeException.class, () -> personService.modify(1L, mockPersonDto()));
+    }
+
+    @Test
+    void modifyIfNameIsDifferent() {
+
+        //When
+        when(personRepository.findById(1L))
+                .thenReturn(Optional.of(new Person("tony")));
+
+        //Then
+        assertThrows(RuntimeException.class, () -> personService.modify(1L, mockPersonDto()));
+    }
+
+    @Test
+    void modify() {
+
+        //Given
+        when(personRepository.findById(1L))
+                .thenReturn(Optional.of(new Person("junhyuk")));
+
+        //When
+        personService.modify(1L, mockPersonDto());
+
+        //Then
+        verify(personRepository, times(1)).save(any(Person.class));
+    }
+
+    private PersonDto mockPersonDto() {
+
+        return PersonDto.of("junhyuk", "programming", "seoul", LocalDate.now(), "programmer", "010-1111-1119");
     }
 }
