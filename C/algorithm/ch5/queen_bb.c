5648a0d8928954f6b5a42e87ed78515cd8ee3f85
junto <takeoutmoneywallet@naver.com>
2021-01-06 19:19:45 +0900

    feat: Solve eight_queen 3steps

---
+++ C/algorithm/ch5/queen_bb.c
@@ -0,0 +1,43 @@
+//
+// Created by junto on 21. 1. 6..
+//
+
+#include <stdio.h>
+
+int flag[8];
+int pos[8];
+
+void print(void)
+{
+    int i;
+    for (i = 0; i < 8; i++)
+        printf("%2d", pos[i]);
+    putchar('\n');
+}
+
+void set(int i)
+{
+    int j;
+    for (j = 0; j < 8; j++) {
+        if (!flag[j]) {
+            pos[i] = j;
+            if (i == 7)
+                print();
+            else {
+                flag[j] = 1;
+                set(i + 1);
+                flag[j] = 0;
+            }
+        }
+    }
+}
+
+int main(void)
+{
+    int i;
+    for (i = 0; i < 8; i++)
+        flag[i] = 0;
+    set(0);
+
+    return 0;
+}
