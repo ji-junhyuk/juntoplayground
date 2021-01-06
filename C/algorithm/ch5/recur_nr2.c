000647b869b0d786702a064a749904fe41623c29
junto <takeoutmoneywallet@naver.com>
2021-01-06 13:48:02 +0900

    feat: Use stack in recur function

---
+++ C/algorithm/ch5/recur_nr2.c
@@ -0,0 +1,35 @@
+//
+// Created by junto on 21. 1. 6..
+//
+#include <stdio.h>
+#include "IntStack.h"
+
+void recur(int n)
+{
+    IntStack stk;
+    Initialize(&stk, 100);
+
+    Top :
+    if (n > 0) {
+        Push(&stk, n);
+        n = n - 1;
+        goto Top;
+    }
+    if (!IsEmpty(&stk)) {
+        Pop(&stk, &n);
+        printf("%d\n", n);
+        n = n - 2;
+        goto Top;
+    }
+
+    Terminate(&stk);
+}
+int main(void)
+{
+    int x;
+    printf("Enter the number:");
+    scanf("%d", &x);
+    recur(x);
+
+    return 0;
+}
