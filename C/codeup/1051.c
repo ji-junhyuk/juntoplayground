820442cc210444231955da281abea6cb56c4d25d
junto <takeoutmoneywallet@naver.com>
2021-01-07 19:26:46 +0900

    feat: Study codeup (1047) ~1054 (25m)

---
+++ C/codeup/1051.c
@@ -0,0 +1,15 @@
+//
+// Created by junto on 21. 1. 4..
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    if (a <= b)
+        printf("%d", 1);
+    else
+        printf("%d", 0);
+}
---
+++ C/codeup/1053.c
@@ -0,0 +1,15 @@
+//
+// Created by junto on 21. 1. 4..
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    if (a == 0)
+        printf("%d", 1);
+    else
+        printf("%d", 0);
+}
