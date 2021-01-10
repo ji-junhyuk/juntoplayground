f6e2b75405122777a5b1573905f7f77484da0134
junto <takeoutmoneywallet@naver.com>
2021-01-10 11:38:07 +0900

    feat: Find string length 3ways

---
+++ C/algorithm/ch8/str.len2.c
@@ -0,0 +1,22 @@
+//
+// Created by junto on 21. 1. 10..
+//
+
+#include <stdio.h>
+
+int str_len(const char *s)
+{
+    int len = 0;
+
+    while (*s++)
+        len++;
+    return len;
+}
+
+int main(void)
+{
+    char str[256];
+    printf("String: ");
+    scanf("%s", str);
+    printf("Length is %d\n", str_len(str));
+}
