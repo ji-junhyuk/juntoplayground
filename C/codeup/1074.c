30c7b7420af60b1c9d38bccab14cce07efb3945b
junto <takeoutmoneywallet@naver.com>
2021-01-11 18:19:49 +0900

    feat: Study codeup (1073) ~1076 (25m)

---
+++ C/codeup/1074.c
@@ -0,0 +1,14 @@
+//
+// Created by junto on 21. 1. 9..
+//
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    scanf("%d", &n);
+    while (n != 0) {
+        printf("%d\n", n);
+        n--;
+    }
+}
---
+++ C/codeup/1076.c
@@ -0,0 +1,15 @@
+//
+// Created by junto on 21. 1. 9..
+//
+#include <stdio.h>
+
+int main(void)
+{
+    char a, i = 97;
+    scanf("%c", &a);
+
+     while (i <= a)
+         printf("%c ", i++);
+
+    return 0;
+}
