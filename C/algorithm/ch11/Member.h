5e61450a175f38d968fdaa288ba56603bc621d4a
junto <takeoutmoneywallet@naver.com>
2021-01-13 15:03:11 +0900

    feat: Practice chain hash

---
+++ C/algorithm/ch11/Member.h
@@ -0,0 +1,25 @@
+//
+// Created by junto on 21. 1. 13..
+//
+
+#ifndef ___Member
+#define ___Member
+
+typedef struct {
+    int no;
+    char name[20];
+} Member;
+
+#define MEMBER_NO 1
+#define MEMBER_NAME 2
+
+int MemberNoCmp(const Member *x, const Member *y);
+
+int MemberNameCmp(const Member *x, const Member *y);
+
+void PrintMember(const Member *x);
+
+void PrintLnMember(const Member *x);
+
+Member ScanMember(const char *message, int sw);
+#endif
