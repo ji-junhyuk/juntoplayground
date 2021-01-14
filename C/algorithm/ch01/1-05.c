09352dbc30152ab076ae6c4dd66f100f23d5e931
junto <takeoutmoneywallet@naver.com>
2021-01-14 14:01:23 +0900

    feat: Find median

---
+++ C/algorithm/ch01/1-05.c
@@ -0,0 +1,10 @@
+//
+// Created by junto on 21. 1. 14..
+//
+#include <stdio.h>
+
+/*
+The condition is judged as duplicate because the
+[else if statement] is the reverse of the judgment of
+the first [if statement]
+*/
