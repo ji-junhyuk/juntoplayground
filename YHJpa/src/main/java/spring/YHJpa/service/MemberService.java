3dce6b4a5ddc965355ce569714a9b0a995544eaa
junto <takeoutmoneywallet@naver.com>
2021-01-18 04:28:51 +0900

    feat: Study YHJpa2 Lecture (20) 23/23

--- YHJpa/src/main/java/spring/YHJpa/service/MemberService.java
+++ YHJpa/src/main/java/spring/YHJpa/service/MemberService.java
@@ -1,7 +1,6 @@
 package spring.YHJpa.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import spring.YHJpa.domain.Member;
