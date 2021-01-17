299f33d882722c15e164162a3a17c7a7f2a14220
junto <takeoutmoneywallet@naver.com>
2021-01-18 07:54:32 +0900

    feat: Use genuinely recursive, another recur function

---
+++ C/algorithm/ch05/1-4.c
@@ -0,0 +1,30 @@
+//
+// Created by junto on 21. 1. 18..
+//
+
+#include <stdio.h>
+
+void recur(int n)
+{
+    if (n > 0) {
+        recur(n - 2);
+        printf("%d\n", n);
+        recur(n - 1);
+    }
+}
+
+int main(void)
+{
+    int x;
+    scanf("%d", &x);
+    recur(x);
+
+    return 0;
+}
+
+/***
+ top-down
+ bottom-up
+ */
+
+
