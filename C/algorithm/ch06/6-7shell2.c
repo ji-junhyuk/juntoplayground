d4dc12c3e1045ec8fb5868d5b4a2321f31d4bc4d
junto <takeoutmoneywallet@naver.com>
2021-01-19 18:22:51 +0900

    feat: Use shell sort

---
+++ C/algorithm/ch06/6-7shell2.c
@@ -0,0 +1,44 @@
+//
+// Created by junto on 21. 1. 19..
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#define swap(type, x, y) do { type t = x; x = y; y = t;} while(0)
+
+void shell(int a[], int n)
+{
+    int i, j, h;
+
+    for (h =1; h < n / 9; h = h * 3 + 1);
+
+    for (; h > 0; h /= 3)
+        for (i = h; i < n; i++) {
+            int tmp = a[i];
+            for (j = i - h; j >= 0 && a[j] > tmp; j -= h)
+                a[j + h] = a[j];
+            a[j + h] = tmp;
+        }
+}
+
+int main(void)
+{
+    int i, nx;
+    int *x;
+
+    scanf("%d", &nx);
+    x = calloc(nx, sizeof(int));
+
+    for (i = 0; i < nx; i++) {
+        scanf("%d", &x[i]);
+    }
+
+    shell(x, nx);
+
+    for (i = 0; i < nx; i++)
+        printf("%d\n", x[i]);
+
+    free(x);
+
+    return 0;
+}
