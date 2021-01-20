0a65d90fcb04fd80a22fff02e81728f05aeb759b
junto <takeoutmoneywallet@naver.com>
2021-01-21 05:35:00 +0900

    feat: Study YHJpa2 Lecture (0) 43/43 (105+70m)

---
+++ YHJpa/src/main/java/spring/YHJpa/api/OrderSimpleApiController.java
@@ -0,0 +1,78 @@
+package spring.YHJpa.api;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import spring.YHJpa.domain.Address;
+import spring.YHJpa.domain.Order;
+import spring.YHJpa.domain.OrderSearch;
+import spring.YHJpa.domain.OrderStatus;
+import spring.YHJpa.repository.OrderRepository;
+import spring.YHJpa.repository.order.simplequery.OrderSimpleQueryDto;
+import spring.YHJpa.repository.order.simplequery.OrderSimpleQueryRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+        }
+        return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+        return orderSimpleQueryRepository.findOrderDtos();
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
+    }
+}
---
+++ YHJpa/src/main/java/spring/YHJpa/repository/order/simplequery/OrderSimpleQueryDto.java
@@ -0,0 +1,25 @@
+package spring.YHJpa.repository.order.simplequery;
+
+import lombok.Data;
+import spring.YHJpa.domain.Address;
+import spring.YHJpa.domain.OrderStatus;
+
+import java.time.LocalDateTime;
+
+@Data
+public class OrderSimpleQueryDto {
+
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+
+    public OrderSimpleQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+    }
+}
