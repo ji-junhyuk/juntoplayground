60e27ca73f0af0e74d486136f2fb998fbd76ea49
junto <takeoutmoneywallet@naver.com>
2021-01-22 03:35:43 +0900

    feat: Use spring mvc

---
+++ YHIntro/src/main/java/spring/YHIntro/controller/HelloController.java
@@ -0,0 +1,45 @@
+package spring.YHIntro.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!~");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    @Getter
+    @Setter
+    static class Hello {
+        private String name;
+    }
+}
