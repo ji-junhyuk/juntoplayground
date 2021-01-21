b818510ac4e68d7ab2b544978346f31ae048a5a5
junto <takeoutmoneywallet@naver.com>
2021-01-22 05:40:09 +0900

    feat: Convert Spring Di to java Di

--- YHBasic/src/main/java/spring/YHBasic/MemberApp.java
+++ YHBasic/src/main/java/spring/YHBasic/MemberApp.java
@@ -1,5 +1,7 @@
 package spring.YHBasic;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import spring.YHBasic.domain.Grade;
 import spring.YHBasic.domain.Member;
 import spring.YHBasic.service.MemberService;
@@ -8,8 +10,10 @@ import spring.YHBasic.service.MemberServiceImpl;
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
--- YHBasic/src/main/java/spring/YHBasic/OrderApp.java
+++ YHBasic/src/main/java/spring/YHBasic/OrderApp.java
@@ -1,5 +1,7 @@
 package spring.YHBasic;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import spring.YHBasic.domain.Grade;
 import spring.YHBasic.domain.Member;
 import spring.YHBasic.domain.Order;
@@ -11,9 +13,12 @@ import spring.YHBasic.service.OrderServiceImpl;
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
