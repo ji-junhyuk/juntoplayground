ffc7746b52785c2ba7aef66f5cb42605bf20570b
junto <takeoutmoneywallet@naver.com>
2021-01-21 19:26:32 +0900

    feat: Use strstr function

---
+++ C/algorithm/ch08/9-6strstr_test.c
@@ -0,0 +1,29 @@
+//
+// Created by junto on 21. 1. 10..
+//
+
+#include <stdio.h>
+#include <string.h>
+
+int main (void)
+{
+    char s1[256], s2[256];
+    char *p;
+    puts("strstr function");
+    printf("text: ");
+    scanf("%s", s1);
+    printf("pattern: ");
+    scanf("%s", s2);
+    p = strstr(s1, s2);
+
+    if (p == NULL)
+        printf("no pattern in text\n");
+    else {
+        int ofs = p - s1;
+        printf("\n%s\n", s1);
+        printf("%*s|\n", ofs, "");
+        printf("%*s%s\n", ofs, "", s2);
+    }
+
+    return 0;
+}
