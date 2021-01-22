65e5c8e210367dd8a2b1f1729d133704909d4d3d
junto <takeoutmoneywallet@naver.com>
2021-01-23 04:39:22 +0900

    Issue: Not Working cancelOrder(Junit5)

--- YHJpa/src/main/java/spring/YHJpa/domain/Order.java
+++ YHJpa/src/main/java/spring/YHJpa/domain/Order.java
@@ -49,4 +49,35 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("Products that have already been shipped cannot be canceled.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
+
 }
---
+++ YHJpa/src/main/java/spring/YHJpa/service/OrderService.java
@@ -0,0 +1,52 @@
+package spring.YHJpa.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import spring.YHJpa.domain.*;
+import spring.YHJpa.domain.item.Item;
+import spring.YHJpa.repository.ItemRepository;
+import spring.YHJpa.repository.MemberRepository;
+import spring.YHJpa.repository.OrderRepository;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final MemberRepository memberRepository;
+    private final OrderRepository orderRepository;
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+        delivery.setStatus(DeliveryStatus.COMP);
+
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        orderRepository.save(order);
+        return order.getId();
+    }
+
+    @Transactional
+    public void cancelOrder(Long orderId) {
+
+        Order order = orderRepository.findOne(orderId);
+        order.cancel();
+    }
+
+//    public List<Order> findOrders(OrderSearch orderSearch) {
+//        return orderRepository.findAllByString(orderSearch);
+//    }
+
+}
