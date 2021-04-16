b9039a50289bcc2509dad822331df59e3257da19
junto <takeoutmoneywallet@naver.com>
2021-04-17 07:59:15 +0900

    feat: Run ch11. 해시 (180m)

---
+++ books/자료 구조와 함께 배우는 알고리즘 - 보요 시바타/ch11/ex06_ClosedHash.h
@@ -0,0 +1,36 @@
+#ifndef CLOSEDHASH
+#define CLOSEDHASH
+
+#include "../ch09/Member.h"
+
+typedef enum 
+{
+	Occupied, Empty, Deleted
+} Status;
+
+typedef struct
+{
+	Member data;
+	Status stat;
+} Bucket;
+
+typedef struct 
+{
+	int size;
+	Bucket *table;
+} ClosedHash;
+
+int Initialize(ClosedHash *h, int size);
+
+Bucket *Search(const ClosedHash *h, const Member *x);
+
+int Add(ClosedHash *h, const Member *x);
+
+int Remove(ClosedHash *h, const Member *x);
+
+void Dump(const ClosedHash *h);
+
+void Clear(ClosedHash *h);
+
+void Terminate(ClosedHash *h);
+#endif
