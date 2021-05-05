9eea9df9fe39ca80832dfaf97a4bfdbf8323a1cf
junto <takeoutmoneywallet@naver.com>
2021-05-05 16:06:48 +0900

    feat: Run 42 / 400 (2m)

---
+++ books/python/ch01/ex14_print_stars1.py
@@ -0,0 +1,11 @@
+print('*를 출력합니다.')
+n = int(input('몇 개를 출력할까요?: '))
+w = int(input('몇 개마다 줄바꿈할까요?: '))
+
+for i in range(n):
+    print('*', end='')
+    if i % w == w - 1:
+        print()
+
+if n % w:
+    print()
