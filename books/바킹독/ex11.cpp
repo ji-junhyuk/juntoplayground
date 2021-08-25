009c33cbd126e6d8a035b4a5c52fa8629ece874b
ji-junhyuk <takeoutmoneywallet@naver.com>
2021-08-25 17:16:54 +0900

    feat: Run

---
+++ books/바킹독/ex11.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> v1 = {1, 2, 3, 4, 5, 6};
+
+for(int e : v1)
+    cout << e << ' ';
+
+for(int i = 0; i < v1.size(); i++)
+    cout << v1[i] << ' ';
+/*
+for(int i = 0; i <= v1.size()-1; i++)
+    cout << v1[i] << ' ';
+*/
