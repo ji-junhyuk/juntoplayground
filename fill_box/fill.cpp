4c400c62afb97e9946166d63d9993e563822fc27
ji-junhyuk <takeoutmoneywallet@naver.com>
2022-02-13 22:52:14 +0900

    feat: finish fill_box

--- fill_box/fill.cpp
+++ fill_box/fill.cpp
@@ -15,7 +15,7 @@ int check(char *arr)
 		{
 			if (jdx < 4)
 			{
-				if (arr[idx * 5 + jdx] == 'X')
+				if (arr[idx * 5 + jdx] == 'O')
 				{
 					flag = 1;
 					return flag;
@@ -29,14 +29,19 @@ int check(char *arr)
 int main(void)
 {
 	char 	arr[25];
+	char	star_arr[25];
 	char 	star_index;
 	char	input;
+	int		flag;
 
+	flag = 24;
 	srand(time(NULL));
 	star_index = 24;
 	for (int idx = 0; idx < 25; idx ++)
-		arr[idx] = 'X';
-	arr[24] = '*';
+		arr[idx] = 'O';
+	for (int idx = 0; idx < 25; idx ++)
+		star_arr[idx] = 'O';
+	arr[24] = 'X';
 
 
 	//cout << "check, arr:" << check(arr);
@@ -47,14 +52,17 @@ int main(void)
 		{
 			for (int jdx = 0; jdx < 5; jdx++)
 			{
+				if (idx * 5 + jdx != flag)
 					cout << arr[idx * 5 + jdx] << '\t';
+				else
+					cout << "X\t";
 			}
 			cout << '\n';
 		}
 
 		cout << "w : 위 s : 아래 a : 왼쪽 d : 오른쪽 q : 종료\n";
 		input = _getch();
-		arr[star_index] = 'O';
+		arr[star_index] = '*';
 		if (input == 'q' || input == 'Q')
 			break ;
 		switch (input)
@@ -63,32 +71,36 @@ int main(void)
 			case 'w':
 				if (star_index > 4)
 				{
-					arr[star_index - 5] = 'O';
+					arr[star_index - 5] = '*';
 					star_index -= 5;
+					flag = star_index;
 				}
 				break;
 			case 'S':
 			case 's':
 				if (star_index < 20)
 				{
-					arr[star_index + 5] = 'O';
+					arr[star_index + 5] = '*';
 					star_index += 5;
+					flag = star_index;
 				}
 				break;
 			case 'A':
 			case 'a':
 				if (star_index % 5 > 0)
 				{
-					arr[star_index - 1 ] = 'O';
+					arr[star_index - 1 ] = '*';
 					star_index -= 1;
+					flag = star_index;
 				}
 				break;
 			case 'D':
 			case 'd':
 				if (star_index % 5 < 4)
 				{
-					arr[star_index + 1] = 'O';
+					arr[star_index + 1] = '*';
 					star_index += 1;
+					flag = star_index;
 				}
 				break;
 		}
