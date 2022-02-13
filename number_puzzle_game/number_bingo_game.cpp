904758d17ef83b868e1306ae7d69af8a7b11cd40
ji-junhyuk <takeoutmoneywallet@naver.com>
2022-02-13 17:52:41 +0900

    feat: add exe, obj file

--- number_puzzle_game/number_bingo_game.cpp
+++ number_puzzle_game/number_bingo_game.cpp
@@ -6,23 +6,48 @@
 
 #include <iostream>
 #include <limits.h>
+#include <conio.h>
 
 using namespace std;
 
+int check(int *arr)
+{
+	int flag;
+
+	flag = 0;
+	for (int idx = 0; idx < 5; idx++)
+	{
+		for (int jdx = 0; jdx < 5; jdx++)
+		{
+			if (jdx < 4)
+			{
+				if (arr[idx * 5 + jdx] > arr[idx * 5 + jdx + 1])
+				{
+					flag = 1;
+					return flag;
+				}
+			}
+		}
+	}
+	return (flag);
+}
+
 int main(void)
 {
-	int arr[25];
+	int 	arr[25];
+	int 	star_index;
+	char	input;
 
 	srand(time(NULL));
+	star_index = 24;
 
 	for (int idx = 0; idx < 24; idx ++)
 		arr[idx] = idx + 1;
 	arr[24] = INT_MAX;
 
-	for (int idx = 0; idx < 25; idx++)
-		cout << arr[idx] << ' ';
-	cout << '\n';
 
+	cout << "check, arr:" << check(arr);
+	cout << '\n';
 	for (int idx = 0; idx < 100; idx++)
 	{
 		int temp;
@@ -37,15 +62,72 @@ int main(void)
 		arr[b] = temp;
 	}
 
-	for (int idx = 0; idx < 5; idx++)
+	for (int idx = 0; idx < 25; idx++)
+		cout << arr[idx] << ' ';
+	cout << "check, arr:" << check(arr);
+	cout << '\n';
+	while (1)
 	{
-		for (int jdx = 0; jdx < 5; jdx++)
+		for (int idx = 0; idx < 5; idx++)
 		{
-			if (arr[idx * 5 + jdx] == INT_MAX)
-				cout << "*\t";
-			else
-				cout << arr[idx * 5 + jdx] << '\t';
+			for (int jdx = 0; jdx < 5; jdx++)
+			{
+				if (arr[idx * 5 + jdx] == INT_MAX)
+					cout << "*\t";
+				else
+					cout << arr[idx * 5 + jdx] << '\t';
+			}
+			cout << '\n';
 		}
-		cout << '\n';
+
+		cout << "w : 위 s : 아래 a : 왼쪽 d : 오른쪽 q : 종료\n";
+		input = _getch();
+		if (input == 'q' || input == 'Q')
+			break ;
+		switch (input)
+		{
+			case 'W':
+			case 'w':
+				if (star_index > 4)
+				{
+					arr[star_index] = arr[star_index - 5];
+					arr[star_index - 5] = INT_MAX;
+					star_index -= 5;
+				}
+				break;
+			case 'S':
+			case 's':
+				if (star_index < 20)
+				{
+					arr[star_index] = arr[star_index + 5];
+					arr[star_index + 5] = INT_MAX;
+					star_index += 5;
+				}
+				break;
+			case 'A':
+			case 'a':
+				if (star_index % 5 > 0)
+				{
+					arr[star_index] = arr[star_index - 1];
+					arr[star_index - 1] = INT_MAX;
+					star_index -= 1;
+				}
+				break;
+			case 'D':
+			case 'd':
+				if (star_index % 5 < 4)
+				{
+					arr[star_index] = arr[star_index + 1];
+					arr[star_index + 1] = INT_MAX;
+					star_index += 1;
+				}
+				break;
+		}
+		if (!check(arr))
+		{
+			cout << "퍼즐을 맞추느라 수고하셨떠염\n";
+			break ;
+		}
 	}
+	cout << "end\n";
 }
