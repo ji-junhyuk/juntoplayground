c37f09a15e86bab42dc5bbb18b25b2105c4d9e0a
ji-junhyuk <takeoutmoneywallet@naver.com>
2022-02-13 18:22:36 +0900

    run

---
+++ fill_box/fill.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <limits.h>
+#include <conio.h>
+
+using namespace std;
+
+int check(char *arr)
+{
+	int flag;
+
+	flag = 0;
+	for (int idx = 0; idx < 5; idx++)
+	{
+		for (int jdx = 0; jdx < 5; jdx++)
+		{
+			if (jdx < 4)
+			{
+				if (arr[idx * 5 + jdx] == 'X')
+				{
+					flag = 1;
+					return flag;
+				}
+			}
+		}
+	}
+	return (flag);
+}
+
+int main(void)
+{
+	char 	arr[25];
+	char 	star_index;
+	char	input;
+
+	srand(time(NULL));
+	star_index = 24;
+	for (int idx = 0; idx < 25; idx ++)
+		arr[idx] = 'X';
+	arr[24] = '*';
+
+
+	//cout << "check, arr:" << check(arr);
+	cout << '\n';
+	while (1)
+	{
+		for (int idx = 0; idx < 5; idx++)
+		{
+			for (int jdx = 0; jdx < 5; jdx++)
+			{
+					cout << arr[idx * 5 + jdx] << '\t';
+			}
+			cout << '\n';
+		}
+
+		cout << "w : 위 s : 아래 a : 왼쪽 d : 오른쪽 q : 종료\n";
+		input = _getch();
+		arr[star_index] = 'O';
+		if (input == 'q' || input == 'Q')
+			break ;
+		switch (input)
+		{
+			case 'W':
+			case 'w':
+				if (star_index > 4)
+				{
+					arr[star_index - 5] = 'O';
+					star_index -= 5;
+				}
+				break;
+			case 'S':
+			case 's':
+				if (star_index < 20)
+				{
+					arr[star_index + 5] = 'O';
+					star_index += 5;
+				}
+				break;
+			case 'A':
+			case 'a':
+				if (star_index % 5 > 0)
+				{
+					arr[star_index - 1 ] = 'O';
+					star_index -= 1;
+				}
+				break;
+			case 'D':
+			case 'd':
+				if (star_index % 5 < 4)
+				{
+					arr[star_index + 1] = 'O';
+					star_index += 1;
+				}
+				break;
+		}
+		if (!check(arr))
+		{
+		for (int idx = 0; idx < 5; idx++)
+		{
+			for (int jdx = 0; jdx < 5; jdx++)
+			{
+					cout << arr[idx * 5 + jdx] << '\t';
+			}
+			cout << '\n';
+		}
+			cout << "퍼즐을 맞추느라 수고하셨떠염\n";
+			break ;
+		}
+	}
+	cout << "end\n";
+}	
