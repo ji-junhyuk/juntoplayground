f0531f5477d4b46de7f36c27e77eb40d1e18f2c1
ji-junhyuk <takeoutmoneywallet@naver.com>
2022-03-11 17:30:35 +0900

    feat: run doubble buffering

--- astr/windows_api_lecture/Client/CCore.cpp
+++ astr/windows_api_lecture/Client/CCore.cpp
@@ -10,12 +10,16 @@ CCore::CCore()
 	: m_hWnd(0)
 	, m_ptResolution{}
 	, m_hDc(0)
+	, m_hBit(0)
+	, m_memDC(0)
 {
 }
 
 CCore::~CCore()
 {
 	ReleaseDC(m_hWnd, m_hDc);
+	DeleteDC(m_memDC);
+	DeleteObject(m_hBit);
 }
 
 int CCore::init(HWND _hWnd, POINT _ptResolution)
@@ -29,6 +33,13 @@ int CCore::init(HWND _hWnd, POINT _ptResolution)
 
 	m_hDc = GetDC(m_hWnd);
 	
+	// 이중 버퍼링 용도의 비트맵과 DC를 만든다.
+	m_hBit = CreateCompatibleBitmap(m_hDc, m_ptResolution.x, m_ptResolution.y);
+	m_memDC = CreateCompatibleDC(m_hDc);
+
+	HBITMAP hOldBit = (HBITMAP)SelectObject(m_memDC, m_hBit);
+	DeleteObject(hOldBit);
+
 	CTimeMgr::GetInst()->init();
 	CKeyMgr::GetInst()->init();
 	g_obj.SetPos(Vec2((float)(m_ptResolution.x / 2), (float)(m_ptResolution.y / 2)));
@@ -60,10 +71,13 @@ void CCore::update()
 
 void CCore::render()
 {
+	// 화면 Clear
+	Rectangle(m_memDC, -1, -1, m_ptResolution.x + 1, m_ptResolution.y + 1);
 	Vec2 vPos = g_obj.GetPos();
 	Vec2 vScale = g_obj.GetScale();
-	Rectangle(m_hDc, int(vPos.x - vScale.x / 2.f)
+	Rectangle(m_memDC, int(vPos.x - vScale.x / 2.f)
 		, int(vPos.y - vScale.y / 2.f)
 		, int(vPos.x + vScale.x / 2.f)
 		, int(vPos.y + vScale.y / 2.f));
+	BitBlt(m_hDc, 0, 0, m_ptResolution.x, m_ptResolution.y, m_memDC, 0, 0, SRCCOPY);
 }
--- astr/windows_api_lecture/Client/CCore.h
+++ astr/windows_api_lecture/Client/CCore.h
@@ -7,6 +7,10 @@ private:
 	HWND	m_hWnd;			// 메인 윈도우 핸들
 	POINT	m_ptResolution;	// 메인 윈도우 해상도
 	HDC		m_hDc;			// 메인 윈도우에 Draw 함수
+
+	HBITMAP	m_hBit;
+	HDC		m_memDC;
+
 public:
 	int init(HWND _hWnd, POINT _ptResolution);
 	void progress();
