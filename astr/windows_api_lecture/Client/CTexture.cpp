08768bb60a5c92bb03a7a75d3fe1872669155bc1
ji-junhyuk <takeoutmoneywallet@naver.com>
2022-03-13 13:38:40 +0900

    feat: set Texture, Sound class

---
+++ astr/windows_api_lecture/Client/CTexture.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include "CTexture.h"
+
+CTexture::CTexture()
+	: m_hBit(0)
+	, m_dc(0)
+{
+}
+
+CTexture::~CTexture()
+{
+}
---
+++ astr/windows_api_lecture/Client/CTexture.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "CRes.h"
+class CTexture :
+    public CRes
+{
+private:
+    HDC     m_dc;
+    HBITMAP m_hBit;
+
+public:
+    CTexture();
+    ~CTexture();
+};
+
